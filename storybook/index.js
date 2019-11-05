91bfb7ef4fc242b66626baca65d6eb9fb0515a4f
yehjxraymond <yehjxraymond@gmail.com>
2019-11-05 12:48:02 +0800

    chore: added eslint (#3)

--- storybook/index.js
+++ storybook/index.js
@@ -1,10 +1,12 @@
 import { AppRegistry } from 'react-native';
+// eslint-disable-next-line import/no-extraneous-dependencies
 import { getStorybookUI, configure } from '@storybook/react-native';
 
 import './rn-addons';
 
 // import stories
 configure(() => {
+  // eslint-disable-next-line global-require
   require('./stories');
 }, module);
 
@@ -12,7 +14,6 @@ configure(() => {
 // To find allowed options for getStorybookUI
 const StorybookUIRoot = getStorybookUI({});
 
-// If you are using React Native vanilla and after installation you don't see your app name here, write it manually.
 // If you use Expo you can safely remove this line.
 AppRegistry.registerComponent('%APP_NAME%', () => StorybookUIRoot);
 
--- storybook/stories/index.js
+++ storybook/stories/index.js
@@ -1,9 +1,10 @@
-import React from "react";
-import { storiesOf } from "@storybook/react-native";
+/* eslint-disable import/no-extraneous-dependencies, import/no-unresolved */
+import React from 'react';
+import { storiesOf } from '@storybook/react-native';
 
-import DocumentRenderer from "../../src/components/DocumentRenderer";
-import demoOc from "./demo-oc.json";
+import DocumentRenderer from '../../src/components/DocumentRenderer';
+import demoOc from './demo-oc.json';
 
-storiesOf("DocumentRenderer", module).add("Demo OpenCerts", () => (
+storiesOf('DocumentRenderer', module).add('Demo OpenCerts', () => (
   <DocumentRenderer document={demoOc} />
 ));
