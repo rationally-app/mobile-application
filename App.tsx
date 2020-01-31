18905035dc03c9ece79d296686a0a8da92eca859
Raymond Yeh <ray@geek.sg>
2020-01-31 22:59:05 +0800

    chore: remove storybook

--- App.tsx
+++ App.tsx
@@ -1,8 +1,6 @@
 import AppNavigation from "./src/navigation";
-import Storybook from "./storybook";
-import { IS_STORYBOOK_VIEW } from "./src/config";
 import { YellowBox } from "react-native";
 
 YellowBox.ignoreWarnings(["Setting a timer"]);
 
-export default IS_STORYBOOK_VIEW ? Storybook : AppNavigation;
+export default AppNavigation;
