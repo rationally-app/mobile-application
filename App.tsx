88d5caddd34c884df60f16d5542b3fd20c559d69
Raymond Yeh <ray@geek.sg>
2020-02-01 11:15:31 +0800

    feat: production logging with sentry

--- App.tsx
+++ App.tsx
@@ -1,6 +1,12 @@
 import AppNavigation from "./src/navigation";
-import { YellowBox } from "react-native";
+import * as Sentry from "sentry-expo";
+import Constants from "expo-constants";
 
-YellowBox.ignoreWarnings(["Setting a timer"]);
+Sentry.init({
+  dsn: "https://2b6599445c3946aeaf4dd31cafc56378@sentry.io/2140518",
+  enableInExpoDevelopment: true,
+  debug: true
+});
+Sentry.setRelease(Constants.manifest.revisionId!);
 
 export default AppNavigation;
