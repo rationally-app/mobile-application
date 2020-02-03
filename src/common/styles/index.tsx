700e90965fc05b870e2d7965d8205c39a77fe362
Raymond Yeh <ray@geek.sg>
2020-02-03 16:58:46 +0800

    chore: removed colorName export

--- src/common/styles/index.tsx
+++ src/common/styles/index.tsx
@@ -1,15 +1,7 @@
-import { color, ColorName } from "./colors";
+import { color } from "./colors";
 import { shadow } from "./shadows";
 import { size } from "./sizes";
 import { letterSpacing, fontSize } from "./typography";
 import { borderRadius } from "./borders";
 
-export {
-  color,
-  shadow,
-  size,
-  letterSpacing,
-  fontSize,
-  borderRadius,
-  ColorName
-};
+export { color, shadow, size, letterSpacing, fontSize, borderRadius };
