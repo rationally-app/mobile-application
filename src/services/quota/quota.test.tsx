974afdbf4f766a1abc462f297f02fdfa5ad72b6b
Raymond Yeh <ray@geek.sg>
2020-02-13 15:01:29 +0800

    fix: lint

--- src/services/quota/quota.test.tsx
+++ src/services/quota/quota.test.tsx
@@ -21,45 +21,47 @@ const mockPostTransactionResponse = {
   transactions: mockGetQuotaResponse
 };
 
-beforeEach(() => {
-  mockFetch.mockReset();
-});
-
-describe("getQuota", () => {
-  it("should return the quota of a nric number", async () => {
-    expect.assertions(2);
-    mockFetch.mockReturnValueOnce({
-      then: () => mockGetQuotaResponse
-    });
-    const quota = await getQuota("S000000J", "KEY", AppMode.staging);
-    expect(mockFetch.mock.calls[0]).toEqual([
-      `${STAGING_ENDPOINT}/quota/S000000J`,
-      { method: "GET", headers: { Authorization: "KEY" } }
-    ]);
-    expect(quota).toEqual(mockGetQuotaResponse);
+describe("quota", () => {
+  beforeEach(() => {
+    mockFetch.mockReset();
   });
-});
 
-describe("postTransaction", () => {
-  it("should create a new transaction", async () => {
-    expect.assertions(2);
-    mockFetch.mockReturnValueOnce({
-      then: () => mockPostTransactionResponse
+  describe("getQuota", () => {
+    it("should return the quota of a nric number", async () => {
+      expect.assertions(2);
+      mockFetch.mockReturnValueOnce({
+        then: () => mockGetQuotaResponse
+      });
+      const quota = await getQuota("S000000J", "KEY", AppMode.staging);
+      expect(mockFetch.mock.calls[0]).toEqual([
+        `${STAGING_ENDPOINT}/quota/S000000J`,
+        { method: "GET", headers: { Authorization: "KEY" } }
+      ]);
+      expect(quota).toEqual(mockGetQuotaResponse);
     });
-    const history = await postTransaction({
-      nric: "S000000J",
-      key: "KEY",
-      transactions: [{ sku: "abc123", quantity: 2 }],
-      mode: AppMode.staging
+  });
+
+  describe("postTransaction", () => {
+    it("should create a new transaction", async () => {
+      expect.assertions(2);
+      mockFetch.mockReturnValueOnce({
+        then: () => mockPostTransactionResponse
+      });
+      const history = await postTransaction({
+        nric: "S000000J",
+        key: "KEY",
+        transactions: [{ sku: "abc123", quantity: 2 }],
+        mode: AppMode.staging
+      });
+      expect(mockFetch.mock.calls[0]).toEqual([
+        `${STAGING_ENDPOINT}/transactions/S000000J`,
+        {
+          method: "POST",
+          headers: { Authorization: "KEY" },
+          body: JSON.stringify([{ sku: "abc123", quantity: 2 }])
+        }
+      ]);
+      expect(history).toEqual(mockPostTransactionResponse);
     });
-    expect(mockFetch.mock.calls[0]).toEqual([
-      `${STAGING_ENDPOINT}/transactions/S000000J`,
-      {
-        method: "POST",
-        headers: { Authorization: "KEY" },
-        body: JSON.stringify([{ sku: "abc123", quantity: 2 }])
-      }
-    ]);
-    expect(history).toEqual(mockPostTransactionResponse);
   });
 });
