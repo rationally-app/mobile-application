263309f01c8b8c1cb56d03cd376d0141063942ac
Sebastian Quek <sebq13@gmail.com>
2020-02-17 10:39:08 +0800

    feat: add Checkbox component

---
+++ src/components/Layout/Checkbox.tsx
@@ -0,0 +1,79 @@
+import React, { FunctionComponent, ReactElement } from "react";
+import { TouchableHighlight, View, StyleSheet } from "react-native";
+import { size, color, borderRadius } from "../../common/styles";
+import { Feather } from "@expo/vector-icons";
+
+const styles = StyleSheet.create({
+  outerWrapper: {
+    paddingHorizontal: size(3)
+  },
+  innerWrapper: {
+    flexDirection: "row",
+    paddingVertical: size(2),
+    borderBottomColor: color("grey", 20),
+    borderBottomWidth: 1
+  },
+  labelWrapper: {
+    flex: 1
+  },
+  toggle: {
+    borderWidth: 1,
+    width: size(6),
+    height: size(6),
+    borderRadius: borderRadius(3),
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  toggleUnchecked: {
+    backgroundColor: "white",
+    borderColor: color("grey", 30)
+  },
+  toggleChecked: {
+    backgroundColor: color("green", 20),
+    borderColor: color("green", 30)
+  }
+});
+
+interface Toggle {
+  isChecked: boolean;
+}
+
+const Toggle: FunctionComponent<Toggle> = ({ isChecked }) => {
+  return (
+    <View
+      style={[
+        styles.toggle,
+        isChecked ? styles.toggleChecked : styles.toggleUnchecked
+      ]}
+    >
+      {isChecked && (
+        <Feather name="check" size={size(3)} color={color("green", 40)} />
+      )}
+    </View>
+  );
+};
+
+interface Checkbox extends Toggle {
+  label: ReactElement;
+  onToggle: (isChecked: boolean) => void;
+}
+
+export const Checkbox: FunctionComponent<Checkbox> = ({
+  label,
+  isChecked,
+  onToggle
+}) => {
+  return (
+    <TouchableHighlight
+      style={styles.outerWrapper}
+      onPress={() => onToggle(!isChecked)}
+      underlayColor={color("grey", 20) + "33"}
+      activeOpacity={1}
+    >
+      <View style={styles.innerWrapper}>
+        <View style={styles.labelWrapper}>{label}</View>
+        <Toggle isChecked={isChecked} />
+      </View>
+    </TouchableHighlight>
+  );
+};
---
+++ storybook/stories/Layout/Checkbox.tsx
@@ -0,0 +1,25 @@
+import React, { useState, ReactElement } from "react";
+import { storiesOf } from "@storybook/react-native";
+import { Checkbox } from "../../../src/components/Layout/Checkbox";
+import { AppText } from "../../../src/components/Layout/AppText";
+import { View } from "react-native";
+import { color } from "../../../src/common/styles";
+
+function CheckboxItem(): ReactElement {
+  const [isChecked, setIsChecked] = useState(false);
+  return (
+    <Checkbox
+      label={<AppText>isChecked: {isChecked.toString()}</AppText>}
+      isChecked={isChecked}
+      onToggle={setIsChecked}
+    />
+  );
+}
+
+storiesOf("Layout", module).add("Checkbox", () => (
+  <View style={{ backgroundColor: color("grey", 10) }}>
+    <CheckboxItem />
+    <CheckboxItem />
+    <CheckboxItem />
+  </View>
+));
