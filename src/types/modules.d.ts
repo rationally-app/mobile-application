fd38064c5b8e82f399784a7250ca220709ac1479
Sebastian Quek <sebq13@gmail.com>
2020-04-15 11:24:59 +0800

    chore: remove module definition

--- src/types/modules.d.ts
+++ src/types/modules.d.ts
@@ -3,11 +3,3 @@ declare module "*.svg" {
   const content: React.StatelessComponent<SvgProps>;
   export default content;
 }
-
-declare module "react-native-dotenv" {
-  export const SENTRY_ORG: string;
-  export const SENTRY_PROJECT: string;
-  export const SENTRY_AUTH_TOKEN: string;
-  export const SENTRY_DSN: string;
-  export const ENV: "dev" | "prod";
-}
