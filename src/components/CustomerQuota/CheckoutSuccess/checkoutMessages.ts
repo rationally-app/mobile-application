0f66f6c2a6209818da55e4687a560f1feb5bc47a
dalsontws <dalsontws@gmail.com>
2020-06-12 18:30:56 +0800

    style: amend next identity button

--- src/components/CustomerQuota/CheckoutSuccess/checkoutMessages.ts
+++ src/components/CustomerQuota/CheckoutSuccess/checkoutMessages.ts
@@ -11,10 +11,14 @@ export const getCheckoutMessages = (productType?: string): CheckoutMessage => {
         title: "Redeemed!",
         description: "Item(s) redeemed:",
 <<<<<<< HEAD
+<<<<<<< HEAD
         ctaButtonText: "Next identity"
 =======
         ctaButtonText: "Next citizen"
 >>>>>>> 167b27e... style: amend checkout message
+=======
+        ctaButtonText: "Next identity"
+>>>>>>> 0a5abdb... style: amend next identity button
       };
     case "purchase":
     default:
