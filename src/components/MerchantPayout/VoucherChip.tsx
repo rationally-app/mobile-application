4d976b4b251d3b744aaac0f97a52cd5b58ca63be
justussoh <justus.soh@hotmail.com>
2020-06-16 19:10:29 +0800

    fix: removed invalid chip

--- src/components/MerchantPayout/VoucherChip.tsx
+++ src/components/MerchantPayout/VoucherChip.tsx
@@ -25,51 +25,31 @@ const styles = StyleSheet.create({
   numVouchersWrapper: {
     paddingHorizontal: size(1.5),
     paddingVertical: size(1),
-    borderRadius: borderRadius(2)
-  },
-  validNumVouchersWrapper: {
+    borderRadius: borderRadius(2),
     backgroundColor: color("blue-green", 40)
-  },
-  invalidNumVouchersWrapper: {
-    backgroundColor: color("red", 60)
   }
 });
 
 interface VoucherChip {
-  valid?: boolean;
   numVouchers: number;
 }
 
 export const VoucherChip: FunctionComponent<VoucherChip> = ({
-  valid = false,
   numVouchers
 }) => {
   return (
     <View>
       <View style={styles.validTextWrapper}>
         <View style={styles.validIcon}>
-          {valid ? (
-            <Feather
-              name="check-circle"
-              size={size(2)}
-              color={color("blue", 50)}
-            />
-          ) : (
-            <Feather name="x-circle" size={size(2)} color={color("blue", 50)} />
-          )}
+          <Feather
+            name="check-circle"
+            size={size(2)}
+            color={color("blue", 50)}
+          />
         </View>
-        <AppText style={styles.validText}>
-          {valid ? "Valid" : "Invalid"}
-        </AppText>
+        <AppText style={styles.validText}>Valid</AppText>
       </View>
-      <View
-        style={[
-          styles.numVouchersWrapper,
-          valid
-            ? styles.validNumVouchersWrapper
-            : styles.invalidNumVouchersWrapper
-        ]}
-      >
+      <View style={styles.numVouchersWrapper}>
         <AppText style={styles.numVouchers}>
           {`${numVouchers} | $${numVouchers * 2}`}
         </AppText>
