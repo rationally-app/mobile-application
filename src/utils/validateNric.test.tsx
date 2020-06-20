ffc959a526761809ef710faaacffb80a88dc5af7
Sebastian Quek <sebq13@gmail.com>
2020-06-20 13:16:05 +0800

    chore: update tests

--- src/utils/validateNric.test.tsx
+++ src/utils/validateNric.test.tsx
@@ -1,20 +1,21 @@
 import { validate } from "./validateNric";
 
 describe("validate", () => {
-  it("should return true for valid nric number with S/T/F/G", () => {
+  it("should return true for valid nric number", () => {
     expect.assertions(4);
-    expect(validate("S6099517F")).toBe(true);
-    expect(validate("T1145794G")).toBe(true);
-    expect(validate("F3151357W")).toBe(true);
-    expect(validate("G2821461N")).toBe(true);
+    // https://en.wikipedia.org/wiki/National_Registration_Identity_Card
+    expect(validate("S0000001I")).toBe(true);
+    expect(validate("S0000002G")).toBe(true);
+    expect(validate("S0000003E")).toBe(true);
+    expect(validate("S0000004C")).toBe(true);
   });
 
   it("should return false for invalid NRIC", () => {
     expect.assertions(5);
-    expect(validate("S6099518F")).toBe(false);
-    expect(validate("T1145795G")).toBe(false);
-    expect(validate("F3151358W")).toBe(false);
-    expect(validate("G2821462N")).toBe(false);
+    expect(validate("S0000001A")).toBe(false);
+    expect(validate("S0000002B")).toBe(false);
+    expect(validate("S0000003C")).toBe(false);
+    expect(validate("S0000004D")).toBe(false);
     expect(validate("2821462")).toBe(false);
   });
 });
