2af53650e27f4cb36a4295f151b293fdf5a6d68c
justussoh <justus.soh@hotmail.com>
2020-06-30 14:54:12 +0800

    try: ci/cd on PR

--- scripts/commenter.sh
+++ scripts/commenter.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-PULL_REQUEST_ID=$(echo $CI_PULL_REQUEST | cut -c 59-)
-GITHUB_PROJECT="rationally-app/mobile-application"
-EXPO_PROJECT="@musket/rationally"
+PULL_REQUEST_ID=$(echo ${{ github.event.pull_request.head.ref }})
+GITHUB_PROJECT="justussoh/mobile-application"
+EXPO_PROJECT="@justussoh/rationally"
+GITHUB_TOKEN=$(echo ${{ secrets.GITHUB_TOKEN }})
 
 PUBLISH_TEXT="Published to https://exp.host/${EXPO_PROJECT}?release-channel=${PULL_REQUEST_ID}"
 STORYBOOK_PUBLISH_TEXT="Published to https://exp.host/${EXPO_PROJECT}?release-channel=storybook-${PULL_REQUEST_ID}"
@@ -9,6 +10,6 @@ QR_CODE="https://api.qrserver.com/v1/create-qr-code/?size=250x250&data=exp://exp
 QR_CODE_STORYBOOK="https://api.qrserver.com/v1/create-qr-code/?size=250x250&data=exp://exp.host/${EXPO_PROJECT}?release-channel=storybook-${PULL_REQUEST_ID}"
 
 curl -X POST \
-  https://${GITHUB_ACCESS_TOKEN_COMMENTER}@api.github.com/repos/${GITHUB_PROJECT}/issues/${PULL_REQUEST_ID}/comments \
+  https://{GITHUB_TOKEN}@api.github.com/repos/${GITHUB_PROJECT}/issues/${PULL_REQUEST_ID}/comments \
   -H 'Content-Type: application/json' \
   -d '{ "body": "## Application\n![Expo QR]('"$QR_CODE"')\n'"$PUBLISH_TEXT"'\n\n## Storybook\n![Expo QR]('"$QR_CODE_STORYBOOK"')\n'"$STORYBOOK_PUBLISH_TEXT"'" }'
