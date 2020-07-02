bf9138ecbcf11cb6b67a7c882f900f6ccf557380
Sebastian Quek <sebq13@gmail.com>
2020-07-03 00:11:10 +0800

    feat: update merchant code validation

--- src/utils/validateMerchantCode.test.tsx
+++ src/utils/validateMerchantCode.test.tsx
@@ -2,16 +2,20 @@ import { validateMerchantCode } from "./validateMerchantCode";
 
 describe("validate merchant code", () => {
   it("should return true for valid merchant code", () => {
-    expect.assertions(5);
-    expect(validateMerchantCode("100000001")).toBe(true);
-    expect(validateMerchantCode("202006171")).toBe(true);
-    expect(validateMerchantCode("merchant code")).toBe(true);
-    expect(validateMerchantCode("merchant-code-a123")).toBe(true);
-    expect(validateMerchantCode("merchant_code_a123")).toBe(true);
+    expect.assertions(9);
+    expect(validateMerchantCode("12345678")).toBe(true);
+    expect(validateMerchantCode("123")).toBe(true);
+    expect(validateMerchantCode("1")).toBe(true);
+    expect(validateMerchantCode("ABCDEFGH")).toBe(true);
+    expect(validateMerchantCode("ABC")).toBe(true);
+    expect(validateMerchantCode("A")).toBe(true);
+    expect(validateMerchantCode("ABCD1234")).toBe(true);
+    expect(validateMerchantCode("A12")).toBe(true);
+    expect(validateMerchantCode("123ABC")).toBe(true);
   });
 
   it("should return false for invalid merchant code", () => {
-    expect.assertions(4);
+    expect.assertions(5);
     expect(() => validateMerchantCode("")).toThrow("Invalid merchant code");
     expect(() => validateMerchantCode("() => {}")).toThrow(
       "Invalid merchant code"
@@ -20,6 +24,9 @@ describe("validate merchant code", () => {
       "Invalid merchant code"
     );
     expect(() => validateMerchantCode("å∫ç∂´ƒ©˙")).toThrow(
+      "Invalid merchant code"
+    );
+    expect(() => validateMerchantCode("ABCD12345678")).toThrow(
       "Invalid merchant code"
     );
   });
