93b6f87cf499fda2de82339978d10ec05511f8d2
justussoh <justus.soh@hotmail.com>
2020-07-24 18:44:30 +0800

    fix: linting error

--- app.config.ts
+++ app.config.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 // Typing for @expo/config is currently experimental(https://docs.expo.io/workflow/configuration/#using-typescript-for-configuration-appconfigts-instead-of)
 // Install when on stable release (https://www.npmjs.com/package/@expo/config)
 const getValue = (value: string | undefined, message: string): string => {
