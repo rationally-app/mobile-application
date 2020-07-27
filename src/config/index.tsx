89e1899ec4937c8f3589ca079facee1ea03e5eaf
justussoh <justus.soh@hotmail.com>
2020-07-27 16:14:13 +0800

    fix: test

--- src/config/index.tsx
+++ src/config/index.tsx
@@ -1,6 +1,6 @@
 import Constants from "expo-constants";
 
 export const IS_STORYBOOK_VIEW =
-  Constants.manifest.extra.storybook ||
+  Constants.manifest?.extra?.storybook ||
   (Constants.manifest?.releaseChannel || "").indexOf("storybook") > -1;
-export const IS_MOCK = Constants.manifest.extra.mock;
+export const IS_MOCK = Constants.manifest?.extra?.mock;
