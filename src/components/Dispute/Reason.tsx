1e21a7571e6bf90e2b194b2c39231503737e26e3
Justin Cheong <justin_cheong@tech.gov.sg>
2020-07-29 04:15:30 +0800

    update with reason alert

--- src/components/Dispute/Reason.tsx
+++ src/components/Dispute/Reason.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, View, TouchableOpacity } from "react-native";
 import React, { FunctionComponent } from "react";
 import { size, color } from "../../common/styles";
 import { AppText } from "../Layout/AppText";
@@ -6,20 +6,40 @@ import { reduce } from "lodash";
 
 const styles = StyleSheet.create({
   reasonComponent: {
+    margin: 0,
     marginBottom: size(2),
     marginHorizontal: -size(2),
     paddingVertical: 21,
     backgroundColor: color("grey", 10)
+  },
+  reasonLayout: {
+    flexDirection: "row",
+    marginHorizontal: 0,
+    marginBottom: 0
+  },
+  reasonAlert: {
+    marginLeft: 10,
+    fontStyle: "italic",
+    color: color("red", 50)
   }
 });
 
 export const Reason: FunctionComponent<{
   description: string;
+  alert?: string;
   isLast: boolean;
-}> = ({ description, isLast }) => {
+}> = ({ description, alert, isLast }) => {
   return (
-    <View style={[styles.reasonComponent, isLast ? { marginBottom: 0 } : {}]}>
-      <AppText>{description}</AppText>
-    </View>
+    <TouchableOpacity
+      style={[styles.reasonComponent, isLast ? { marginBottom: 0 } : {}]}
+      onPress={() => {
+        console.warn(`${description}`);
+      }}
+    >
+      <View style={styles.reasonLayout}>
+        <AppText>{description}</AppText>
+        <AppText style={styles.reasonAlert}>{alert ?? ""}</AppText>
+      </View>
+    </TouchableOpacity>
   );
 };
