0e9ca29abe9738762dcd366c5de1d91c0c65fa59
Shaodian Wang <diandianws@gmail.com>
2020-07-29 22:55:57 +0800

    fix: remove unnecessary parenthesis

--- src/components/CustomerQuota/CheckoutSuccess/RedeemedItem.tsx
+++ src/components/CustomerQuota/CheckoutSuccess/RedeemedItem.tsx
@@ -26,14 +26,12 @@ export const RedeemedItem: FunctionComponent<{
       <View style={sharedStyles.itemRow}>
         <AppText style={sharedStyles.itemHeaderText}>{categoryName}</AppText>
       </View>
-      {
-        <View style={sharedStyles.quantitiesWrapper}>
-          <View style={sharedStyles.quantitiesBorder} />
-          <AppText style={sharedStyles.quantityByIdText}>
-            {identifierInputDisplay ?? quantitiesDisplay}
-          </AppText>
-        </View>
-      }
+      <View style={sharedStyles.quantitiesWrapper}>
+        <View style={sharedStyles.quantitiesBorder} />
+        <AppText style={sharedStyles.quantityByIdText}>
+          {identifierInputDisplay ?? quantitiesDisplay}
+        </AppText>
+      </View>
     </View>
   );
 };
