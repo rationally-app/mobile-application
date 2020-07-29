83b3a4c9ead84f50ef05dfd78677d31979718355
Sebastian Quek <sebq13@gmail.com>
2020-07-29 14:05:41 +0800

    fix: ensure when constants is undefined, nothing blows up

--- src/config/index.tsx
+++ src/config/index.tsx
@@ -4,6 +4,6 @@ export const IS_STORYBOOK_VIEW =
   Constants.manifest?.extra?.storybook ||
   (Constants.manifest?.releaseChannel || "").indexOf("storybook") > -1;
 export const IS_MOCK = Constants.manifest?.extra?.mock;
-export const APP_BINARY_VERSION = Constants.manifest.version;
+export const APP_BINARY_VERSION = Constants.manifest?.version;
 export const APP_BUILD_VERSION: number =
-  Constants.manifest.extra.appBuildVersion;
+  Constants.manifest?.extra?.appBuildVersion;
