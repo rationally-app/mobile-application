c6d10980da4a2cd75f4b90e4f4c84cdfe06e9f29
ddwsd <diandianws@gmail.com>
2020-08-03 18:28:00 +0800

    style: remove comment for old code

--- src/components/Login/LoginContainer.tsx
+++ src/components/Login/LoginContainer.tsx
@@ -96,7 +96,6 @@ export const InitialisationContainer: FunctionComponent<NavigationProps> = ({
   const resetStage = (): void => {
     setLoginStage("SCAN");
   };
-  // const [lastResendWarningMessage, setLastResendWarningMessage] = useState("");
 
   const handleLogout = useCallback((): void => {
     logout(navigation.dispatch);
