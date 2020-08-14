522e07a0252ff0b13965c243c1ac26a95ecf6d12
dalsontws <dalsontws@gmail.com>
2020-08-14 18:46:26 +0800

    feat: change error messages for qr, use new alert modal

--- src/components/Login/utils.ts
+++ src/components/Login/utils.ts
@@ -12,13 +12,19 @@ interface DecodedQrResponse {
 export const decodeQr = (code: string): DecodedQrResponse => {
   try {
     const parsedCode: QrCode = JSON.parse(code);
-    if (!parsedCode.endpoint) throw new Error("No endpoint specified");
-    if (!parsedCode.key) throw new Error("No key specified");
+    if (!parsedCode.endpoint)
+      throw new Error(
+        "We are currently facing login issues. Get a new QR code from your in-charge."
+      );
+    if (!parsedCode.key)
+      throw new Error(
+        "We are currently facing login issues. Get a new QR code from your in-charge."
+      );
     return { endpoint: parsedCode.endpoint, key: parsedCode.key };
   } catch (e) {
     if (e.message.includes("Unexpected token"))
       throw new Error(
-        "Invalid QR code format, consider regenerating your QR code"
+        "Scan QR code again or get a new QR code from your in-charge."
       );
     throw e;
   }
