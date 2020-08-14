fe239369d8faa643a82f67f03d5039f8c8bc905c
dalsontws <dalsontws@gmail.com>
2020-08-14 18:54:06 +0800

    test: fix qr error messages

--- src/components/Login/utils.test.tsx
+++ src/components/Login/utils.test.tsx
@@ -5,7 +5,7 @@ describe("decodeQr", () => {
     expect.assertions(1);
     const code = "1e4457bc-f7d0-4329-a344-f0e3c75d8dd4";
     expect(() => decodeQr(code)).toThrow(
-      "Invalid QR code format, consider regenerating your QR code"
+      "Scan QR code again or get a new QR code from your in-charge."
     );
   });
 
@@ -20,9 +20,13 @@ describe("decodeQr", () => {
   it("should throw if the code can be parsed but does not contain the right fields", () => {
     expect.assertions(2);
     const missingKey = `{"keys": "1e4457bc-f7d0-4329-a344-f0e3c75d8dd4","endpoint": "https://somewhere.com"}`;
-    expect(() => decodeQr(missingKey)).toThrow("No key specified");
+    expect(() => decodeQr(missingKey)).toThrow(
+      "We are currently facing login issues. Get a new QR code from your in-charge."
+    );
 
     const missingEndpoint = `{"key": "1e4457bc-f7d0-4329-a344-f0e3c75d8dd4","endpointed": "https://somewhere.com"}`;
-    expect(() => decodeQr(missingEndpoint)).toThrow("No endpoint specified");
+    expect(() => decodeQr(missingEndpoint)).toThrow(
+      "We are currently facing login issues. Get a new QR code from your in-charge."
+    );
   });
 });
