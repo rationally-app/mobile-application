64560c03f1e1f45b94423472124b6fb49b58a290
dalsontws <dalsontws@gmail.com>
2020-08-20 15:19:45 +0800

    chore: add nric validation error messages in enum

--- src/utils/validateNric.tsx
+++ src/utils/validateNric.tsx
@@ -1,4 +1,5 @@
 // Thanks to https://gist.github.com/kyrene-chew/6f275325335ab27895beb7a9a7b4c1cb
+import { ERROR_MESSAGE } from "../context/alert";
 
 const NRIC_REGEX = /^(\D)(\d{7})(\D)$/;
 
@@ -52,8 +53,8 @@ export const validate = (nricInput: string): boolean => {
 
 export const validateAndCleanNric = (inputNric: string): string => {
   const isNricValid = validate(inputNric);
-  if (!isNricValid) throw new Error("Enter or scan ID number again.");
+  if (!isNricValid) throw new Error(ERROR_MESSAGE.INVALID_ID);
   const cleanedNric = inputNric.match(NRIC_REGEX)?.[0].toUpperCase();
-  if (!cleanedNric) throw new Error("Enter or scan ID number again.");
+  if (!cleanedNric) throw new Error(ERROR_MESSAGE.INVALID_ID);
   return cleanedNric;
 };
