70d9bd20a2b3cbfef32c73bd8eb13d1a59e1577f
Sze Ying <ting.szeying@gmail.com>
2020-08-28 22:02:12 +0800

    feat: show more/less toggle

--- src/components/CustomerQuota/NoQuotaCard.tsx
+++ src/components/CustomerQuota/NoQuotaCard.tsx
@@ -1,4 +1,4 @@
-import React, { FunctionComponent, useContext } from "react";
+import React, { FunctionComponent, useContext, useState } from "react";
 import { compareDesc } from "date-fns";
 import { differenceInSeconds, format, formatDistance } from "date-fns";
 import { View, StyleSheet, TouchableOpacity } from "react-native";
@@ -9,15 +9,16 @@ import { sharedStyles } from "./sharedStyles";
 import { DarkButton } from "../Layout/Buttons/DarkButton";
 import { Cart } from "../../hooks/useCart/useCart";
 import { usePastTransaction } from "../../hooks/usePastTransaction/usePastTransaction";
-import { FontAwesome } from "@expo/vector-icons";
 import { Quota } from "../../types";
 import { CampaignConfigContext } from "../../context/campaignConfig";
 import { ProductContext } from "../../context/products";
 import { getAllIdentifierInputDisplay } from "../../utils/getIdentifierInputDisplay";
 import { AuthContext } from "../../context/auth";
+import { FontAwesome, Ionicons } from "@expo/vector-icons";
 import { formatQuantityText } from "./utils";
 
 const DURATION_THRESHOLD_SECONDS = 60 * 10; // 10 minutes
+const MAX_TRANSACTIONS_TO_DISPLAY = 5;
 
 const styles = StyleSheet.create({
   wrapper: {
@@ -54,6 +55,16 @@ const styles = StyleSheet.create({
     marginBottom: 0,
     fontFamily: "brand-bold",
     fontSize: size(2)
+  },
+  showFullListToggleBorder: {
+    borderBottomWidth: 1,
+    borderBottomColor: color("grey", 30),
+    flex: 1
+  },
+  showFullListToggleWrapper: {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center"
   }
 });
 
@@ -105,23 +116,46 @@ const UsageQuotaTitle: FunctionComponent<{
   </>
 );
 
-interface ItemTransaction {
-  name: string;
+interface TransactionsByCategory {
+  category: string;
+  transactions: Transaction[];
+}
+
+interface Transaction {
   transactionDate: string;
   details: string;
   quantity: string;
+  order?: number;
 }
 
-const ItemTransaction: FunctionComponent<ItemTransaction> = ({
-  name,
+const TransactionsByCategory: FunctionComponent<{
+  category: string;
+  transactions: Transaction[];
+  maxTransactionsToDisplay: number;
+}> = ({ category, transactions, maxTransactionsToDisplay }) => {
+  const shouldShowCategory =
+    (transactions[0].order || 0) < maxTransactionsToDisplay;
+  return shouldShowCategory ? (
+    <View style={styles.wrapper}>
+      <View style={styles.itemRow}>
+        <AppText style={styles.itemHeader}>{category}</AppText>
+      </View>
+      {transactions.map(
+        (transaction, index) =>
+          (transaction.order || 0) < maxTransactionsToDisplay && (
+            <Transaction key={index} {...transaction} />
+          )
+      )}
+    </View>
+  ) : null;
+};
+
+const Transaction: FunctionComponent<Transaction> = ({
   transactionDate,
   details,
   quantity
 }) => (
-  <View style={styles.wrapper}>
-    <View style={styles.itemRow}>
-      <AppText style={styles.itemHeader}>{name}</AppText>
-    </View>
+  <>
     <View style={styles.itemRow}>
       <AppText style={styles.itemSubheader}>{transactionDate}</AppText>
       <AppText style={styles.itemSubheader}>{quantity}</AppText>
@@ -132,7 +166,7 @@ const ItemTransaction: FunctionComponent<ItemTransaction> = ({
         <AppText style={styles.itemDetail}>{details}</AppText>
       </View>
     )}
-  </View>
+  </>
 );
 
 const NoPreviousTransactionTitle: FunctionComponent<{
@@ -170,6 +204,32 @@ interface NoQuotaCard {
   quotaResponse: Quota | null;
 }
 
+const ShowFullListToggle: FunctionComponent<{
+  onClick: () => void;
+  displayText: string;
+  icon: any;
+}> = ({ onClick, displayText, icon }) => (
+  <View
+    style={{
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      marginTop: size(2),
+      marginBottom: size(2)
+    }}
+  >
+    <TouchableOpacity
+      onPress={onClick}
+      style={styles.showFullListToggleWrapper}
+    >
+      <View style={styles.showFullListToggleBorder} />
+      {icon}
+      <View style={styles.showFullListToggleBorder} />
+    </TouchableOpacity>
+    <AppText style={styles.itemHeader}>{displayText}</AppText>
+  </View>
+);
+
 /**
  * Shows when the user cannot purchase anything
  *
@@ -182,34 +242,44 @@ export const NoQuotaCard: FunctionComponent<NoQuotaCard> = ({
   onAppeal,
   quotaResponse
 }) => {
+  const [isShowFullList, setIsShowFullList] = useState<boolean>(false);
   const { policies: allProducts } = useContext(CampaignConfigContext);
   const { getProduct } = useContext(ProductContext);
   const { sessionToken, endpoint } = useContext(AuthContext);
 
   const policyType = cart.length > 0 && getProduct(cart[0].category)?.type;
 
-  const itemTransactions: ItemTransaction[] = [];
-  let latestTransactionTime: Date | undefined = new Date();
+  const { pastTransactionsResult } = usePastTransaction(ids, sessionToken, endpoint);
+  // Assumes results are already sorted (valid assumption for results from /transactions/history)
+  const sortedTransactions = pastTransactionsResult?.pastTransactions;
 
-  const { pastTransactionsResult } = usePastTransaction(
-    ids,
-    sessionToken,
-    endpoint
-  );
+  const latestTransactionTime: Date | undefined =
+    sortedTransactions?.[0].transactionTime ?? undefined;
+  const now = new Date();
+  const secondsFromLatestTransaction = latestTransactionTime
+    ? differenceInSeconds(now, latestTransactionTime)
+    : -1;
 
-  const sortedTransactions = pastTransactionsResult?.pastTransactions.sort(
-    (item1, item2) =>
-      compareDesc(item1.transactionTime ?? 0, item2.transactionTime ?? 0)
-  );
-
+  const transactionsByCategoryMap: {
+    [category: string]: {
+      transactions: Transaction[];
+      hasLatestTransaction?: boolean;
+    };
+  } = {};
   sortedTransactions?.forEach(item => {
     const policy = allProducts.find(
       policy => policy.category === item.category
     );
     const categoryName = policy?.name ?? item.category;
     const formattedDate = format(item.transactionTime, "d MMM yyyy, h:mma");
-    itemTransactions.push({
-      name: categoryName,
+
+    if (!(categoryName in transactionsByCategoryMap)) {
+      transactionsByCategoryMap[categoryName] = {
+        transactions: [],
+        hasLatestTransaction: false
+      };
+    }
+    transactionsByCategoryMap[categoryName].transactions.push({
       transactionDate: formattedDate,
       details: getAllIdentifierInputDisplay(item.identifierInputs ?? []),
       quantity: formatQuantityText(
@@ -217,14 +287,48 @@ export const NoQuotaCard: FunctionComponent<NoQuotaCard> = ({
         policy?.quantity.unit || { type: "POSTFIX", label: " qty" }
       )
     });
+    // TODO: check if seconds is good, or need to change to minutes
+    transactionsByCategoryMap[categoryName].hasLatestTransaction =
+      transactionsByCategoryMap[categoryName].hasLatestTransaction ||
+      differenceInSeconds(latestTransactionTime || 0, item.transactionTime) ===
+        0;
   });
 
-  latestTransactionTime = sortedTransactions?.[0].transactionTime ?? undefined;
+  const latestTransactionsByCategory: TransactionsByCategory[] = [];
+  const otherTransactionsByCategory: TransactionsByCategory[] = [];
+  Object.entries(transactionsByCategoryMap).forEach(([key, value]) => {
+    const { transactions, hasLatestTransaction } = value;
+    if (hasLatestTransaction) {
+      latestTransactionsByCategory.push({
+        category: key,
+        transactions: transactions
+      });
+    } else {
+      otherTransactionsByCategory.push({ category: key, transactions });
+    }
+  });
 
-  const now = new Date();
-  const secondsFromLatestTransaction = latestTransactionTime
-    ? differenceInSeconds(now, latestTransactionTime)
-    : -1;
+  const sortTransactionsByCategory = (
+    a: TransactionsByCategory,
+    b: TransactionsByCategory
+  ): 1 | -1 | 0 =>
+    a.category > b.category ? 1 : b.category > a.category ? -1 : 0;
+  latestTransactionsByCategory.sort(sortTransactionsByCategory);
+  otherTransactionsByCategory.sort(sortTransactionsByCategory);
+
+  let transactionCounter = 0;
+  const transactionsByCategoryList = latestTransactionsByCategory
+    .concat(otherTransactionsByCategory)
+    .map(transactionsByCategory => {
+      const orderedTransactions = transactionsByCategory.transactions.map(
+        transaction => {
+          const result = { ...transaction, order: transactionCounter };
+          transactionCounter += 1;
+          return result;
+        }
+      );
+      return { ...transactionsByCategory, transactions: orderedTransactions };
+    });
 
   const hasAppealProduct = (): boolean => {
     return (
@@ -282,16 +386,44 @@ export const NoQuotaCard: FunctionComponent<NoQuotaCard> = ({
                   ) : undefined
               )}
           </AppText>
-          {itemTransactions.length > 0 && (
+          {transactionsByCategoryList.length > 0 && (
             <View>
               <AppText style={styles.wrapper}>
                 Item(s) {policyType === "REDEEM" ? "redeemed" : "purchased"}{" "}
                 previously:
               </AppText>
-              {itemTransactions.map((transaction, index: number) => (
-                <ItemTransaction key={index} {...transaction} />
-              ))}
+              {transactionsByCategoryList.map(
+                (
+                  transactionsByCategory: TransactionsByCategory,
+                  index: number
+                ) => (
+                  <TransactionsByCategory
+                    key={index}
+                    maxTransactionsToDisplay={
+                      isShowFullList ? 99999 : MAX_TRANSACTIONS_TO_DISPLAY
+                    }
+                    {...transactionsByCategory}
+                  />
+                )
+              )}
             </View>
+          )}
+          {transactionCounter > MAX_TRANSACTIONS_TO_DISPLAY && (
+            <ShowFullListToggle
+              onClick={() => setIsShowFullList(!isShowFullList)}
+              displayText={isShowFullList ? "Show less" : "Show more"}
+              icon={
+                <Ionicons
+                  name={
+                    isShowFullList
+                      ? "ios-arrow-dropup-circle"
+                      : "ios-arrow-dropdown-circle"
+                  }
+                  size={size(4)}
+                  color={color("blue", 50)}
+                />
+              }
+            />
           )}
         </View>
       </CustomerCard>
