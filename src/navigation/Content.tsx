61407d8ced30009ac414e969e8477658807d28d1
Matthea Loo <dhemutton@users.noreply.github.com>
2020-09-01 11:08:09 +0800

    fix: edit content drawer nav

--- src/navigation/Content.tsx
+++ src/navigation/Content.tsx
@@ -1,9 +1,10 @@
-import React, { ReactElement, useRef, useEffect } from "react";
+import React, { ReactElement, useEffect } from "react";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import { NavigationContainer } from "@react-navigation/native";
 import {
-  createAppContainer,
-  createSwitchNavigator,
-  NavigationContainerComponent
-} from "react-navigation";
+  createStackNavigator,
+  TransitionPresets
+} from "@react-navigation/stack";
 import CustomerQuotaStack from "./CustomerQuotaStack";
 import MerchantPayoutStack from "./MerchantPayoutStack";
 import { StatusBar, View, Platform } from "react-native";
@@ -11,44 +12,45 @@ import LoginScreen from "./LoginScreen";
 import { useAppState } from "../hooks/useAppState";
 import { useCheckUpdates } from "../hooks/useCheckUpdates";
 import * as Linking from "expo-linking";
-import { createDrawerNavigator } from "react-navigation-drawer";
 import { DrawerNavigationComponent } from "../components/Layout/DrawerNavigation";
-import { StackViewTransitionConfigs } from "react-navigation-stack";
 import { CampaignInitialisationScreen } from "../components/CampaignInitialisation/CampaignInitialisationScreen";
 
-const SwitchNavigator = createSwitchNavigator(
-  {
-    LoginScreen: { screen: LoginScreen, path: "login" },
-    CampaignInitialisationScreen,
-    DrawerNavigator: createDrawerNavigator(
-      {
-        CustomerQuotaStack: {
-          screen: CustomerQuotaStack
-        },
-        MerchantPayoutStack: {
-          screen: MerchantPayoutStack
-        }
-      },
-      {
-        drawerPosition: "right",
-        drawerType: "slide",
-        contentComponent: DrawerNavigationComponent,
-        navigationOptions: {
-          transitionConfig: () => StackViewTransitionConfigs.SlideFromRightIOS,
-          navigationOptions: {
-            gesturesEnabled: true
-          }
-        }
-      }
-    )
-  },
-  { initialRouteName: "LoginScreen" }
-);
+const Stack = createStackNavigator();
+const Drawer = createDrawerNavigator();
 
-const AppContainer = createAppContainer(SwitchNavigator);
+// const SwitchNavigator = createSwitchNavigator(
+//   {
+//     LoginScreen: { screen: LoginScreen, path: "login" },
+//     CampaignInitialisationScreen,
+//     DrawerNavigator: createDrawerNavigator(
+//       {
+//         CustomerQuotaStack: {
+//           screen: CustomerQuotaStack
+//         },
+//         MerchantPayoutStack: {
+//           screen: MerchantPayoutStack
+//         }
+//       },
+//       {
+//         drawerPosition: "right",
+//         drawerType: "slide",
+//         contentComponent: DrawerNavigationComponent,
+//         navigationOptions: {
+//           transitionConfig: () => StackViewTransitionConfigs.SlideFromRightIOS,
+//           navigationOptions: {
+//             gesturesEnabled: true
+//           }
+//         }
+//       }
+//     )
+//   },
+//   { initialRouteName: "LoginScreen" }
+// );
+
+// const AppContainer = createAppContainer(SwitchNavigator);
 
 export const Content = (): ReactElement => {
-  const navigatorRef = useRef<NavigationContainerComponent>(null);
+  const navigatorRef = React.useRef(null);
   const appState = useAppState();
   const prefix = Linking.makeUrl("/");
 
@@ -69,7 +71,42 @@ export const Content = (): ReactElement => {
             Platform.OS === "android" && !__DEV__ ? StatusBar.currentHeight : 0 // padding is used to prevent content from going behind the status bar on Android production builds
         }}
       >
-        <AppContainer ref={navigatorRef} uriPrefix={prefix} />
+        <NavigationContainer
+          ref={navigatorRef}
+          linking={{
+            prefixes: [prefix]
+          }}
+        >
+          <Stack.Navigator
+            initialRouteName="LoginScreen"
+            screenOptions={{
+              gestureEnabled: true,
+              ...TransitionPresets.SlideFromRightIOS
+            }}
+          >
+            <Stack.Screen name="LoginScreen" component={LoginScreen} />
+            <Stack.Screen
+              name="CampaignInitialisationScreen"
+              component={CampaignInitialisationScreen}
+            />
+            <Drawer.Navigator
+              initialRouteName="Home"
+              drawerPosition="right"
+              drawerType="slide"
+              drawerContent={DrawerNavigationComponent}
+            >
+              <Drawer.Screen
+                name="CustomerQuotaStack"
+                component={CustomerQuotaStack}
+              />
+              <Drawer.Screen
+                name="MerchantPayoutStack"
+                component={MerchantPayoutStack}
+              />
+            </Drawer.Navigator>
+          </Stack.Navigator>
+        </NavigationContainer>
+        {/* <AppContainer ref={navigatorRef} uriPrefix={prefix} /> */}
       </View>
     </>
   );
