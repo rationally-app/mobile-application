c3bf8855ac293afb8ef9365b9a871730f874cda4
Sze Ying <ting.szeying@gmail.com>
2020-09-03 13:54:54 +0800

    test: add tests for functions to manipulate transactions

---
+++ src/components/CustomerQuota/NoQuota/NoQuotaCard.test.ts
@@ -0,0 +1,209 @@
+import { toDate } from "date-fns";
+import { defaultIdentifier } from "../../../test/helpers/wrapper";
+import { Policy, PastTransactionsResult } from "../../../types";
+import {
+  groupTransactionsByCategory,
+  sortTransactions,
+  TransactionsByCategoryMap
+} from "./NoQuotaCard";
+
+describe("NoQuotaCard utility functions", () => {
+  let sortedTransactions: PastTransactionsResult["pastTransactions"];
+  let allProducts: Policy[];
+  let mockTransactionsByCategoryMap: TransactionsByCategoryMap;
+
+  const latestTransactionTimeMs = 1596530350000;
+  const latestTransactionTime = toDate(latestTransactionTimeMs);
+
+  beforeAll(() => {
+    sortedTransactions = [
+      {
+        category: "tt-token",
+        quantity: 1,
+        identifierInputs: [
+          {
+            ...defaultIdentifier,
+            label: "Device code",
+            value: "AAA987654321"
+          }
+        ],
+        transactionTime: latestTransactionTime
+      },
+      {
+        category: "meal-credits",
+        quantity: 10,
+        transactionTime: latestTransactionTime
+      },
+      {
+        category: "vouchers",
+        quantity: 1,
+        identifierInputs: [
+          {
+            ...defaultIdentifier,
+            label: "Voucher code",
+            value: "AAA987654322"
+          }
+        ],
+        transactionTime: toDate(latestTransactionTimeMs - 10000)
+      },
+      {
+        category: "meal-credits",
+        quantity: 5,
+        transactionTime: toDate(latestTransactionTimeMs - 20000)
+      }
+    ];
+    allProducts = [
+      {
+        category: "tt-token",
+        name: "TT token",
+        order: 0,
+        quantity: { period: 1, limit: 1 },
+        identifiers: [
+          {
+            label: "Device code",
+            textInput: { disabled: true, visible: true, type: "STRING" },
+            scanButton: { disabled: false, visible: true, type: "QR" }
+          }
+        ]
+      },
+      {
+        category: "vouchers",
+        name: "CDC Vouchers",
+        order: 0,
+        quantity: {
+          period: 1,
+          limit: 1,
+          unit: { type: "POSTFIX", label: " book" }
+        },
+        identifiers: [
+          {
+            label: "Voucher code",
+            textInput: { disabled: false, visible: true, type: "STRING" },
+            scanButton: { disabled: false, visible: true, type: "BARCODE" }
+          }
+        ]
+      },
+      {
+        category: "meal-credits",
+        name: "Meal credits",
+        order: 0,
+        quantity: { period: 1, limit: 20, unit: { type: "PREFIX", label: "$" } }
+      }
+    ];
+
+    mockTransactionsByCategoryMap = {
+      "TT token": {
+        transactions: [
+          {
+            transactionDate: "4 Aug 2020, 4:39PM",
+            details: "AAA987654321",
+            quantity: "1 qty",
+            isAppeal: false
+          }
+        ],
+        hasLatestTransaction: true
+      },
+      "Meal credits": {
+        transactions: [
+          {
+            transactionDate: "4 Aug 2020, 4:39PM",
+            details: "",
+            quantity: "$10",
+            isAppeal: false
+          },
+          {
+            transactionDate: "4 Aug 2020, 4:38PM",
+            details: "",
+            quantity: "$5",
+            isAppeal: false
+          }
+        ],
+        hasLatestTransaction: true
+      },
+      "CDC Vouchers": {
+        transactions: [
+          {
+            transactionDate: "4 Aug 2020, 4:39PM",
+            details: "AAA987654322",
+            quantity: "1 book",
+            isAppeal: false
+          }
+        ],
+        hasLatestTransaction: false
+      }
+    };
+  });
+
+  describe("groupTransactionsByCategory", () => {
+    it("should return categories with latest transactions and other categories", () => {
+      expect.assertions(1);
+
+      expect(
+        groupTransactionsByCategory(
+          sortedTransactions,
+          allProducts,
+          latestTransactionTime
+        )
+      ).toStrictEqual(mockTransactionsByCategoryMap);
+    });
+
+    it("should return empty object if sortedTransactions is null", () => {
+      expect.assertions(1);
+
+      expect(
+        groupTransactionsByCategory(null, allProducts, undefined)
+      ).toStrictEqual({});
+    });
+  });
+
+  describe("sortTransactions", () => {
+    it("should return array of transactions by category, categories with latest transactions should be before the rest", () => {
+      expect.assertions(1);
+      expect(sortTransactions(mockTransactionsByCategoryMap)).toStrictEqual([
+        {
+          category: "Meal credits",
+          transactions: [
+            {
+              transactionDate: "4 Aug 2020, 4:39PM",
+              details: "",
+              quantity: "$10",
+              isAppeal: false,
+              order: 0
+            },
+            {
+              transactionDate: "4 Aug 2020, 4:38PM",
+              details: "",
+              quantity: "$5",
+              isAppeal: false,
+              order: 1
+            }
+          ]
+        },
+        {
+          category: "TT token",
+          transactions: [
+            {
+              transactionDate: "4 Aug 2020, 4:39PM",
+              details: "AAA987654321",
+              quantity: "1 qty",
+              isAppeal: false,
+              order: 2
+            }
+          ]
+        },
+        {
+          category: "CDC Vouchers",
+          transactions: [
+            {
+              transactionDate: "4 Aug 2020, 4:39PM",
+              details: "AAA987654322",
+              quantity: "1 book",
+              isAppeal: false,
+              order: 3
+            }
+          ]
+        }
+      ]);
+    });
+  });
+});
