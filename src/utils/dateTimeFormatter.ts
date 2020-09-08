73fcbe9f3cf4eeeeb866a1378119d3683a56ab98
Sebastian Quek <sebq13@gmail.com>
2020-09-08 18:31:47 +0800

    feat: add formatDate to only format the date and not the time

--- src/utils/dateTimeFormatter.ts
+++ src/utils/dateTimeFormatter.ts
@@ -2,3 +2,6 @@ import { format } from "date-fns";
 
 export const formatDateTime = (date: number | Date): string =>
   format(date, "d MMM yyyy, h:mma");
+
+export const formatDate = (date: number | Date): string =>
+  format(date, "d MMM yyyy");
