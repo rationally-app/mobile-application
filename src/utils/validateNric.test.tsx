3c96783d3a2718b748a505194b55cfa43d8f8f38
Sebastian Quek <sebastianquek@users.noreply.github.com>
2020-09-14 23:38:47 +0800

    fix: improve id validation (#179)
    * fix: ensure ids with trailing chars will be truncated and still pass validation
    
    * refactor: no real need for additional variable
    
    * refactor: switch around string operations

--- src/utils/validateNric.test.tsx
+++ src/utils/validateNric.test.tsx
@@ -1,23 +1,30 @@
-import { validate } from "./validateNric";
+import { validateAndCleanNric } from "./validateNric";
 
-describe("validate", () => {
-  it("should return true for valid nric number", () => {
-    expect.assertions(4);
+describe("validateAndCleanNric", () => {
+  it("should return cleaned nric for valid nric number", () => {
+    expect.assertions(10);
     // https://en.wikipedia.org/wiki/National_Registration_Identity_Card
-    expect(validate("S0000001I")).toBe(true);
-    expect(validate("S0000002G")).toBe(true);
-    expect(validate("S0000003E")).toBe(true);
-    expect(validate("S0000004C")).toBe(true);
+    expect(validateAndCleanNric("S0000001I")).toBe("S0000001I");
+    expect(validateAndCleanNric("S0000002G")).toBe("S0000002G");
+    expect(validateAndCleanNric("S0000003E")).toBe("S0000003E");
+    expect(validateAndCleanNric("S0000004C")).toBe("S0000004C");
+    expect(validateAndCleanNric("  S0000004C")).toBe("S0000004C");
+    expect(validateAndCleanNric("  S0000004C    ")).toBe("S0000004C");
+    expect(validateAndCleanNric("G1234567X")).toBe("G1234567X");
+    expect(validateAndCleanNric("G1234567X150319SVP")).toBe("G1234567X");
+    expect(validateAndCleanNric("S0000001I123A")).toBe("S0000001I");
+    expect(validateAndCleanNric("S0000001I^&*(asd123")).toBe("S0000001I");
   });
 
-  it("should return false for invalid NRIC", () => {
+  it("should throw error for invalid NRIC", () => {
     expect.assertions(7);
-    expect(validate("S0000001A")).toBe(false);
-    expect(validate("123AS0000001A")).toBe(false);
-    expect(validate("S0000001A123A")).toBe(false);
-    expect(validate("S0000002B")).toBe(false);
-    expect(validate("S0000003C")).toBe(false);
-    expect(validate("S0000004D")).toBe(false);
-    expect(validate("2821462")).toBe(false);
+    const errorMessage = "Enter or scan valid ID number.";
+    expect(() => validateAndCleanNric("S0000001A")).toThrow(errorMessage);
+    expect(() => validateAndCleanNric("123AS0000001I")).toThrow(errorMessage);
+    expect(() => validateAndCleanNric("S0000001A123A")).toThrow(errorMessage);
+    expect(() => validateAndCleanNric("S0000002B")).toThrow(errorMessage);
+    expect(() => validateAndCleanNric("S0000003C")).toThrow(errorMessage);
+    expect(() => validateAndCleanNric("S0000004D")).toThrow(errorMessage);
+    expect(() => validateAndCleanNric("2821462")).toThrow(errorMessage);
   });
 });
