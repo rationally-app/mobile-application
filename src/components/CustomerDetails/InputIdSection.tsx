ff99dcfe44eec866bc4ad98523137a79f9a188c4
dalsontws <dalsontws@gmail.com>
2020-09-16 15:25:31 +0800

    feat: shorten horizontal separator between scan and enter id

--- src/components/CustomerDetails/InputIdSection.tsx
+++ src/components/CustomerDetails/InputIdSection.tsx
@@ -14,7 +14,7 @@ const styles = StyleSheet.create({
   },
   horizontalRule: {
     borderBottomColor: color("grey", 30),
-    marginHorizontal: -size(3),
+    marginHorizontal: -size(0.5),
     borderBottomWidth: 1
   },
   orWrapper: {
