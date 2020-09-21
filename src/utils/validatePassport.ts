2d34dc39f908b6286d0c026486f402cf0bce6ce1
Justin Cheong <justin_cheong@tech.gov.sg>
2020-09-21 16:48:36 +0800

    update with passport regex

--- src/utils/validatePassport.ts
+++ src/utils/validatePassport.ts
@@ -1,11 +1,12 @@
 import { ERROR_MESSAGE } from "../context/alert";
 
 const validate = (inputPassport: string): boolean => {
-  return false;
+  const passportRegex = "^[A-Za-z]{1,3}-[a-zA-Z0-9]{8,11}$";
+  return inputPassport.match(passportRegex) !== null;
 };
 
 export const validateAndCleanPassport = (inputPassport: string): string => {
-  const cleanedInputPassport = inputPassport;
+  const cleanedInputPassport = inputPassport.trim().toUpperCase();
   const isPassportValid = validate(cleanedInputPassport);
   if (!isPassportValid) {
     throw new Error(ERROR_MESSAGE.INVALID_ID);
