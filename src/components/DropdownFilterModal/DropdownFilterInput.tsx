7261b1a8fe3d5859496eec693ad756ff3dbeecea
Justin Cheong <justin_cheong@tech.gov.sg>
2020-09-21 14:32:46 +0800

    update styling

--- src/components/DropdownFilterModal/DropdownFilterInput.tsx
+++ src/components/DropdownFilterModal/DropdownFilterInput.tsx
@@ -4,7 +4,27 @@ import { size, fontSize, borderRadius, color } from "../../common/styles";
 import { AppText } from "../Layout/AppText";
 import { DropdownFilterModal, DropdownItem } from "./DropdownFilterModal";
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  label: {
+    fontFamily: "brand-bold"
+  },
+  inputView: {
+    marginTop: size(1),
+    minHeight: size(6),
+    borderRadius: borderRadius(2),
+    borderWidth: 1,
+    backgroundColor: color("grey", 0),
+    borderColor: color("blue", 50)
+  },
+  inputText: {
+    fontFamily: "brand-regular",
+    fontSize: fontSize(0),
+    marginTop: size(1),
+    marginLeft: size(1),
+    alignItems: "center",
+    color: color("blue", 50)
+  }
+});
 
 export interface DropdownFilterInput {
   label: string;
@@ -35,32 +55,14 @@ export const DropdownFilterInput: FunctionComponent<DropdownFilterInput> = ({
         onTitleSelection={onTitleSelection}
         closeModal={closeModal}
       />
-      <AppText style={{ fontFamily: "brand-bold" }}>{label}</AppText>
+      <AppText style={styles.label}>{label}</AppText>
       <View
         onTouchStart={() => {
           setModalVisible(true);
         }}
-        style={{
-          marginTop: size(1),
-          minHeight: size(6),
-          borderRadius: borderRadius(2),
-          borderWidth: 1,
-          backgroundColor: color("grey", 0),
-          borderColor: color("blue", 50)
-        }}
+        style={styles.inputView}
       >
-        <Text
-          style={{
-            fontFamily: "brand-regular",
-            fontSize: fontSize(0),
-            marginTop: size(1),
-            marginLeft: size(1),
-            alignItems: "center",
-            color: color("blue", 50)
-          }}
-        >
-          {value}
-        </Text>
+        <Text style={styles.inputText}>{value}</Text>
       </View>
     </View>
   );
