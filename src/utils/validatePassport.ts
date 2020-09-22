c43ec1077684dbac9959cc07ef18e89cff7515b2
Justin Cheong <justin_cheong@tech.gov.sg>
2020-09-22 12:38:15 +0800

    update storybook

--- src/utils/validatePassport.ts
+++ src/utils/validatePassport.ts
@@ -1,12 +1,8 @@
 import { ERROR_MESSAGE } from "../context/alert";
 
 const validate = (inputPassport: string): boolean => {
-  const passportRegex1 = "^[A-Za-z]{1,3}-[a-zA-Z0-9]{9}$";
-  const passportRegex2 = "^[A-Za-z]{1,3}-[a-zA-Z]{1}[ ]{1}[a-zA-Z0-9]{9}$";
-  return (
-    inputPassport.match(passportRegex1) !== null ||
-    inputPassport.match(passportRegex2) != null
-  );
+  const passportRegex = "^[A-Za-z]{1,3}-[a-zA-Z0-9]{5,9}$";
+  return inputPassport.match(passportRegex) !== null;
 };
 
 export const validateAndCleanPassport = (inputPassport: string): string => {
