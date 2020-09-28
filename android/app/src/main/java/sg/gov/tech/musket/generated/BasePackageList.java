8c1fadd5dc26869c09d4f022e042c257684fac65
Gabriel Tay <gabriel_tay@tech.gov.sg>
2020-09-28 18:34:08 +0800

    install

--- android/app/src/main/java/sg/gov/tech/musket/generated/BasePackageList.java
+++ android/app/src/main/java/sg/gov/tech/musket/generated/BasePackageList.java
@@ -7,7 +7,11 @@ import org.unimodules.core.interfaces.Package;
 public class BasePackageList {
   public List<Package> getPackageList() {
     return Arrays.<Package>asList(
+        new expo.modules.barcodescanner.BarCodeScannerPackage(),
+        new expo.modules.camera.CameraPackage(),
         new expo.modules.constants.ConstantsPackage(),
+        new expo.modules.crypto.CryptoPackage(),
+        new expo.modules.device.DevicePackage(),
         new expo.modules.errorrecovery.ErrorRecoveryPackage(),
         new expo.modules.filesystem.FileSystemPackage(),
         new expo.modules.font.FontLoaderPackage(),
