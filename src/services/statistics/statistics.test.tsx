2d2076a2885acc0bff525e684d79718d5967c1a4
dalsontws <dalsontws@gmail.com>
2020-09-30 11:36:12 +0800

    test: clean up statistics unit tests

--- src/services/statistics/statistics.test.tsx
+++ src/services/statistics/statistics.test.tsx
@@ -1,5 +1,6 @@
 import { getDailyStatistics, StatisticsError } from "./index";
 import { Sentry } from "../../utils/errorTracking";
+import { DailyStatistics } from "../../types";
 
 jest.mock("../../utils/errorTracking");
 const mockCaptureException = jest.fn();
@@ -8,60 +9,68 @@ const mockCaptureException = jest.fn();
 const mockFetch = jest.fn();
 jest.spyOn(global, "fetch").mockImplementation(mockFetch);
 
-const key = "KEY";
-const endpoint = "https://myendpoint.com";
-
-const dailyStatsResult = [
-  {
-    category: "toilet-paper",
-    quantity: 100,
-    transactionTime: Date.now()
-  },
-  {
-    category: "instant-noodles",
-    quantity: 1,
-    transactionTime: Date.now()
-  },
-  {
-    category: "chocolate",
-    quantity: 30,
-    transactionTime: Date.now()
-  },
-  {
-    category: "vouchers",
-    quantity: 1,
-    transactionTime: Date.now()
-  },
-  {
-    category: "vouchers",
-    quantity: 1,
-    transactionTime: Date.now()
-  }
-];
-
-const timestamp = new Date(2020, 3, 1);
-
-const mockDailyStatsResult = {
-  pastTransactions: dailyStatsResult.map(t => ({
-    ...t,
-    transactionTime: timestamp.getTime()
-  }))
-};
-
-const mockDailyStatsResponse = {
-  pastTransactions: dailyStatsResult.map(t => ({
-    ...t,
-    transactionTime: timestamp.getTime()
-  }))
-};
-
 describe("statistics", () => {
+  let key: string;
+  let endpoint: string;
+  let dailyStatsResult;
+  let mockDailyStatsResult: DailyStatistics;
+  let mockDailyStatsResponse: DailyStatistics;
+  let timestamp: Date;
+
   beforeEach(() => {
     mockFetch.mockReset();
     mockCaptureException.mockReset();
   });
 
   describe("getDailyStatistics", () => {
+    beforeAll(() => {
+      key = "KEY";
+      endpoint = "https://myendpoint.com";
+
+      dailyStatsResult = [
+        {
+          category: "toilet-paper",
+          quantity: 100,
+          transactionTime: Date.now()
+        },
+        {
+          category: "instant-noodles",
+          quantity: 1,
+          transactionTime: Date.now()
+        },
+        {
+          category: "chocolate",
+          quantity: 30,
+          transactionTime: Date.now()
+        },
+        {
+          category: "vouchers",
+          quantity: 1,
+          transactionTime: Date.now()
+        },
+        {
+          category: "vouchers",
+          quantity: 1,
+          transactionTime: Date.now()
+        }
+      ];
+
+      timestamp = new Date(2020, 3, 1);
+
+      mockDailyStatsResult = {
+        pastTransactions: dailyStatsResult.map(t => ({
+          ...t,
+          transactionTime: timestamp.getTime()
+        }))
+      };
+
+      mockDailyStatsResponse = {
+        pastTransactions: dailyStatsResult.map(t => ({
+          ...t,
+          transactionTime: timestamp.getTime()
+        }))
+      };
+    });
     it("should return past transactions based on operatorToken", async () => {
       expect.assertions(1);
       mockFetch.mockReturnValueOnce({
