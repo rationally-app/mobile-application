195d2f0d17ab29d99a1eb5f6270b308b3b00c483
dalsontws <dalsontws@gmail.com>
2020-09-30 11:15:51 +0800

    test: clean up reserve coupon utils test cases

--- src/components/DailyStatistics/utils.test.ts
+++ src/components/DailyStatistics/utils.test.ts
@@ -1,143 +1,149 @@
 import { summariseTransactions } from "./utils";
-import { CampaignPolicy } from "../../types";
+import { CampaignPolicy, DailyStatistics } from "../../types";
 
-const pastTransactions = {
-  pastTransactions: [
-    {
-      category: "instant-noodles",
-      quantity: 999,
-      transactionTime: 12000000000
-    },
-    {
-      category: "chocolate",
-      quantity: 3000,
-      transactionTime: 12000000000
-    },
-    {
-      category: "vouchers",
-      quantity: 20,
-      transactionTime: 12000000000
-    }
-  ]
-};
+describe("summariseTransactions", () => {
+  let pastTransactions: DailyStatistics;
+  let campaignPolicy: CampaignPolicy[] = [];
+  let pastInstantNoodleTransactions: DailyStatistics;
+  let invalidPastTransactions: DailyStatistics;
 
-const pastInstantNoodleTransactions = {
-  pastTransactions: [
-    {
-      category: "instant-noodles",
-      quantity: 999,
-      transactionTime: 12000000000
-    }
-  ]
-};
+  beforeAll(() => {
+    pastTransactions = {
+      pastTransactions: [
+        {
+          category: "instant-noodles",
+          quantity: 999,
+          transactionTime: 12000000000
+        },
+        {
+          category: "chocolate",
+          quantity: 3000,
+          transactionTime: 12000000000
+        },
+        {
+          category: "vouchers",
+          quantity: 20,
+          transactionTime: 12000000000
+        }
+      ]
+    };
 
-const invalidPastTransactions = {
-  pastTransactions: [
-    {
-      category: "funny-category",
-      quantity: 999,
-      transactionTime: 12000000000
-    }
-  ]
-};
+    pastInstantNoodleTransactions = {
+      pastTransactions: [
+        {
+          category: "instant-noodles",
+          quantity: 999,
+          transactionTime: 12000000000
+        }
+      ]
+    };
 
-let campaignPolicy: CampaignPolicy[] = [];
-campaignPolicy = [
-  {
-    category: "toilet-paper",
-    name: "🧻 Toilet Paper",
-    description: "1 ply / 2 ply / 3 ply",
-    order: 1,
-    type: "REDEEM",
-    quantity: {
-      period: 7,
-      limit: 2,
-      unit: {
-        type: "POSTFIX",
-        label: " pack(s)"
-      }
-    }
-  },
-  {
-    category: "instant-noodles",
-    name: "🍜 Instant Noodles",
-    description: "Indomee",
-    order: 2,
-    type: "REDEEM",
-    quantity: {
-      period: 30,
-      limit: 1,
-      unit: {
-        type: "POSTFIX",
-        label: " pack(s)"
-      }
-    }
-  },
-  {
-    category: "chocolate",
-    name: "🍫 Chocolate",
-    description: "Dark / White / Assorted",
-    order: 3,
-    type: "REDEEM",
-    quantity: {
-      period: 14,
-      limit: 30,
-      step: 5,
-      unit: {
-        type: "PREFIX",
-        label: "$"
-      }
-    }
-  },
-  {
-    category: "vouchers",
-    name: "Funfair Vouchers",
-    order: 4,
-    type: "REDEEM",
-    quantity: { period: 1, limit: 1, default: 1 },
-    identifiers: [
+    invalidPastTransactions = {
+      pastTransactions: [
+        {
+          category: "funny-category",
+          quantity: 999,
+          transactionTime: 12000000000
+        }
+      ]
+    };
+
+    campaignPolicy = [
       {
-        label: "Voucher",
-        textInput: { visible: true, disabled: false, type: "STRING" },
-        scanButton: {
-          visible: true,
-          disabled: false,
-          type: "BARCODE",
-          text: "Scan"
+        category: "toilet-paper",
+        name: "🧻 Toilet Paper",
+        description: "1 ply / 2 ply / 3 ply",
+        order: 1,
+        type: "REDEEM",
+        quantity: {
+          period: 7,
+          limit: 2,
+          unit: {
+            type: "POSTFIX",
+            label: " pack(s)"
+          }
         }
       },
       {
-        label: "Token",
-        textInput: { visible: true, disabled: true, type: "STRING" },
-        scanButton: {
-          visible: true,
-          disabled: false,
-          type: "QR",
-          text: "Scan"
+        category: "instant-noodles",
+        name: "🍜 Instant Noodles",
+        description: "Indomee",
+        order: 2,
+        type: "REDEEM",
+        quantity: {
+          period: 30,
+          limit: 1,
+          unit: {
+            type: "POSTFIX",
+            label: " pack(s)"
+          }
         }
-      }
-    ]
-  },
-  {
-    category: "voucher",
-    name: "🎟️ Golden Ticket",
-    order: 5,
-    type: "REDEEM",
-    quantity: { period: 1, limit: 1, default: 1 },
-    identifiers: [
+      },
       {
-        label: "Phone number",
-        textInput: { visible: true, disabled: true, type: "PHONE_NUMBER" },
-        scanButton: {
-          visible: false,
-          disabled: false
+        category: "chocolate",
+        name: "🍫 Chocolate",
+        description: "Dark / White / Assorted",
+        order: 3,
+        type: "REDEEM",
+        quantity: {
+          period: 14,
+          limit: 30,
+          step: 5,
+          unit: {
+            type: "PREFIX",
+            label: "$"
+          }
         }
+      },
+      {
+        category: "vouchers",
+        name: "Funfair Vouchers",
+        order: 4,
+        type: "REDEEM",
+        quantity: { period: 1, limit: 1, default: 1 },
+        identifiers: [
+          {
+            label: "Voucher",
+            textInput: { visible: true, disabled: false, type: "STRING" },
+            scanButton: {
+              visible: true,
+              disabled: false,
+              type: "BARCODE",
+              text: "Scan"
+            }
+          },
+          {
+            label: "Token",
+            textInput: { visible: true, disabled: true, type: "STRING" },
+            scanButton: {
+              visible: true,
+              disabled: false,
+              type: "QR",
+              text: "Scan"
+            }
+          }
+        ]
+      },
+      {
+        category: "voucher",
+        name: "🎟️ Golden Ticket",
+        order: 5,
+        type: "REDEEM",
+        quantity: { period: 1, limit: 1, default: 1 },
+        identifiers: [
+          {
+            label: "Phone number",
+            textInput: { visible: true, disabled: true, type: "PHONE_NUMBER" },
+            scanButton: {
+              visible: false,
+              disabled: false
+            }
+          }
+        ]
       }
-    ]
-  }
-];
+    ];
+  });
 
-describe("summariseTransactions", () => {
   it("should return multiple summarised transactions categories with total count and count per category and the name to be displayed on the stats page", () => {
     expect.assertions(1);
     expect(
