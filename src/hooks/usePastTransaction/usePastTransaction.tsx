cacf0c79e64f1265451db0ea9d73ec6bb4fc29e2
Yong Jia Jie <yongjiajie@protonmail.com>
2020-10-01 11:27:48 +0800

    chore: fixed imports

--- src/hooks/usePastTransaction/usePastTransaction.tsx
+++ src/hooks/usePastTransaction/usePastTransaction.tsx
@@ -6,6 +6,7 @@ import {
   PastTransactionError
 } from "../../services/quota";
 import { Sentry } from "../../utils/errorTracking";
+import { ERROR_MESSAGE } from "../../context/alert";
 
 export type PastTransactionHook = {
   pastTransactionsResult: PastTransactionsResult["pastTransactions"] | null;
