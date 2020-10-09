225cdf5f431692827bfc70daedc075b50c4c95a7
Sze Ying <ting.szeying@gmail.com>
2020-10-09 11:51:40 +0800

    fix: default to key when c13n[key] is undefined

--- src/hooks/useTranslate/useTranslate.tsx
+++ src/hooks/useTranslate/useTranslate.tsx
@@ -12,14 +12,14 @@ export type TranslationHook = {
 export const useTranslate = (): TranslationHook => {
   const { c13n } = useContext(CampaignConfigContext);
 
-  const c13nt = (key: string): string => (c13n && key ? c13n[key] : key);
+  const c13nt = (key: string): string => (c13n && key && c13n[key]) ?? key;
 
   const c13ntForUnit = (
     unit: CampaignPolicy["quantity"]["unit"]
   ): CampaignPolicy["quantity"]["unit"] =>
     unit && {
       ...unit,
-      label: c13nt(unit.label)!
+      label: c13nt(unit.label)
     };
 
   return { c13nt, c13ntForUnit };
