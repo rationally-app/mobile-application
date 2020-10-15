41339743470d45f5f70fde738ac455f231f4b151
shermainelim <shermaine.lim.si.hui@gmail.com>
2020-10-15 22:43:54 +0800

    Revert "fix:fix-cancel entry button json"
    This reverts commit e52a922b9630753fac30562903dedf5f8147be0d.

---
+++ .eslintrc.js
@@ -0,0 +1,65 @@
+module.exports = {
+  parser: "@typescript-eslint/parser",
+  env: {
+    browser: true,
+    es6: true
+  },
+  extends: [
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react/recommended",
+    "plugin:jest/all",
+    "plugin:prettier/recommended",
+    "prettier/@typescript-eslint",
+    "prettier/react"
+  ],
+  plugins: ["react", "@typescript-eslint", "react-hooks"],
+  globals: {
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly"
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true
+    }
+  },
+  rules: {
+    "react/jsx-filename-extension": [
+      1,
+      { extensions: [".js", ".jsx", ".ts", ".tsx"] }
+    ],
+    "react/prop-types": 0,
+    "react/jsx-boolean-value": [1, "always"],
+    "import/prefer-default-export": 0,
+    "@typescript-eslint/no-unused-vars": [2, { args: "none" }],
+    "@typescript-eslint/no-explicit-any": 0,
+    "@typescript-eslint/no-non-null-assertion": 0,
+    "@typescript-eslint/explicit-function-return-type": [
+      "warn",
+      {
+        allowExpressions: true,
+        allowTypedFunctionExpressions: true
+      }
+    ],
+    "jest/no-hooks": 0,
+    "jest/lowercase-name": 0,
+    "react-hooks/rules-of-hooks": "error",
+    "react-hooks/exhaustive-deps": "error",
+    "no-duplicate-imports": "error"
+  },
+  overrides: [
+    {
+      files: ["**/*.test.tsx", "storybook/**/*.tsx"],
+      rules: {
+        "@typescript-eslint/no-empty-function": "off",
+        "jest/prefer-strict-equal": "off"
+      }
+    }
+  ],
+  settings: {
+    react: {
+      version: "detect"
+    }
+  }
+};
