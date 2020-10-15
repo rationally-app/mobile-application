d11f7b83808e080f1f16b909d60e46eb83ed4202
shermainelim <shermaine.lim.si.hui@gmail.com>
2020-10-15 22:42:55 +0800

    Revert "fix:cancel button changes"
    This reverts commit 241969b23b941079c499b764a5acdf89eddbbac1.

--- src/common/styles/index.tsx
+++ src/common/styles/index.tsx
@@ -1,15 +1,7 @@
 import { color } from "./colors";
 import { shadow } from "./shadows";
 import { size } from "./sizes";
-import { letterSpacing, fontSize, normalize } from "./typography";
+import { letterSpacing, fontSize } from "./typography";
 import { borderRadius } from "./borders";
 
-export {
-  color,
-  shadow,
-  size,
-  letterSpacing,
-  fontSize,
-  borderRadius,
-  normalize
-};
+export { color, shadow, size, letterSpacing, fontSize, borderRadius };
--- src/common/styles/typography.tsx
+++ src/common/styles/typography.tsx
@@ -1,5 +1,3 @@
-import { Dimensions, PixelRatio, Platform } from "react-native";
-
 export type FontSizeLevel = -4 | -3 | -2 | -1 | 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7;
 
 /**
@@ -26,12 +24,14 @@ export type LetterSpacingLevel = 1 | 2;
  */
 export const letterSpacing = (level: LetterSpacingLevel): number =>
   [0.5, 0.7][level - 1];
+<<<<<<< HEAD
+<<<<<<< HEAD
 
-const { width: SCREEN_WIDTH } = Dimensions.get("window");
+const { width: SCREEN_WIDTH, height: SCREEN_HEIGHT } = Dimensions.get("window");
 console.log(Dimensions.get("window"));
 
 // based on iphone 6s's scale
-const scale = SCREEN_WIDTH / 375;
+const scale = SCREEN_WIDTH / 575;
 
 export const normalize = (size: number): number => {
   const newSize = size * scale;
@@ -41,3 +41,7 @@ export const normalize = (size: number): number => {
     return Math.round(PixelRatio.roundToNearestPixel(newSize)) - 2;
   }
 };
+=======
+>>>>>>> parent of 36bef30... fix: cancel alert fix based on scaling
+=======
+>>>>>>> parent of 36bef30... fix: cancel alert fix based on scaling
