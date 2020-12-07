a0d0b6a48fb58dbcb22b8b7ba11b48c4dab331e6
Andre Hadianto <andrehadiantolesmana@gmail.com>
2020-12-07 17:56:54 +0800

    fix: declaration

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,7 +1,6 @@
 import "@testing-library/jest-native/extend-expect";
 import "isomorphic-fetch";
-
-const mockAsyncStorage = require("@react-native-async-storage/async-storage/jest/async-storage-mock");
+import mockAsyncStorage from "@react-native-async-storage/async-storage/jest/async-storage-mock";
 
 jest.mock("react-native/Libraries/Vibration/Vibration", () => ({
   vibrate: () => "Vibration.vibrate",
