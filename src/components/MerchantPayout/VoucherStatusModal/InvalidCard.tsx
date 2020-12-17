fb0c92192aac9dbde48ecfadac019fb4ab5570d5
shermainelim <shermaine.lim.si.hui@gmail.com>
2020-12-18 01:36:40 +0800

    Update src/components/MerchantPayout/VoucherStatusModal/InvalidCard.tsx
    Co-authored-by: Immanuella Lim <ilhaoni@users.noreply.github.com>

--- src/components/MerchantPayout/VoucherStatusModal/InvalidCard.tsx
+++ src/components/MerchantPayout/VoucherStatusModal/InvalidCard.tsx
@@ -46,7 +46,7 @@ export const InvalidCard: FunctionComponent<InvalidCard> = ({
   return (
     <Card style={styles.card}>
       <Text style={styles.emoji}>❌</Text>
-      <View style={sharedStyles.statusTitleWrapper}>
+      <AppText style={sharedStyles.statusTitleWrapper}>
         {typeof title === "string" ? (
           <AppText style={sharedStyles.statusTitle}>{title}</AppText>
         ) : (
