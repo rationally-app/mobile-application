afc7f79bf5916e5554d6bcc2dc26d6232ad9e6ab
shermainelim <shermaine.lim.si.hui@gmail.com>
2020-12-31 17:31:49 +0800

    feat: caching of assets

--- src/components/FontLoader/index.tsx
+++ src/components/FontLoader/index.tsx
@@ -1,6 +1,12 @@
 import React, { ReactElement, useEffect, useState } from "react";
 import * as Font from "expo-font";
 import { LoadingView } from "../Loading";
+import {
+  Feather,
+  MaterialIcons,
+  FontAwesome,
+  MaterialCommunityIcons,
+} from "@expo/vector-icons";
 
 export const FontLoader = ({
   children,
@@ -11,6 +17,10 @@ export const FontLoader = ({
   useEffect(() => {
     const loadFonts = async (): Promise<void> => {
       await Font.loadAsync({
+        ...Feather.font,
+        ...MaterialIcons.font,
+        ...MaterialCommunityIcons.font,
+        ...FontAwesome.font,
         "brand-regular": require("../../../assets/fonts/IBMPlexSans-Regular.ttf"),
         "brand-italic": require("../../../assets/fonts/IBMPlexSans-Italic.ttf"),
         "brand-bold": require("../../../assets/fonts/IBMPlexSans-Bold.ttf"),
