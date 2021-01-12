ee0cb3e09513a430816e494f9dd49205a1a8e05c
Andre Hadianto <andrehadiantolesmana@gmail.com>
2021-01-12 11:18:41 +0800

    feat: modify mock policies and pasttransactions with tt-token

--- src/services/quota/index.tsx
+++ src/services/quota/index.tsx
@@ -319,6 +319,17 @@ export const mockPastTransactions = async (
   return {
     pastTransactions: [
       {
+        category: "tt-token",
+        quantity: 1,
+        transactionTime,
+        identifierInputs: [
+          {
+            label: "Device code",
+            value: "AA:BB:CC:DD:EE:FF",
+          },
+        ],
+      },
+      {
         category: "toilet-paper",
         quantity: 1,
         transactionTime,
