423065adf3291449825117fe34bd601ed2e3ce7d
manikandan <maksha@outlook.com>
2021-01-19 17:45:31 +0800

    chore : joi replaced by regex for email

--- src/utils/validateEmailAddress.test.tsx
+++ src/utils/validateEmailAddress.test.tsx
@@ -7,11 +7,13 @@ describe("Email validation", () => {
     "test2@gmail.com.sg",
     "      test3@example.com",
     " test4@example.com     ",
+    " test5@example.co.sg",
+    "test-6@example.com",
   ])("validate valid email-address", (emailAddress: string) => {
     expect(validateEmailAddress(emailAddress)).toBe(emailAddress);
   });
 
-  it.each(["@example.com", "rwl", "", 1])(
+  it.each(["@example.com", "rwl", "", "1"])(
     "validate Invalid email-address",
     (emailAddress: any) => {
       const errorMessage = "Enter valid email address";
