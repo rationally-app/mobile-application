26747328c760c104d11e34924fa6d74e24f9bea0
Andre Hadianto <andrehadiantolesmana@gmail.com>
2021-02-02 10:52:51 +0800

    fix: wrong parameter placement

--- src/components/CustomerQuota/NotEligibleCard.tsx
+++ src/components/CustomerQuota/NotEligibleCard.tsx
@@ -21,11 +21,11 @@ const NotEligibleTransactionDescription: FunctionComponent = () => {
   const { i18nt, c13nt } = useTranslate();
   return (
     <AppText style={{ marginBottom: size(1) }}>
-      {`${
-        (c13nt("notEligibleDescription"),
+      {`${c13nt(
+        "notEligibleDescription",
         undefined,
-        i18nt("notEligibleScreen", "logAppeal"))
-      }`}
+        i18nt("notEligibleScreen", "logAppeal")
+      )}`}
     </AppText>
   );
 };
