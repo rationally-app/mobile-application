0ab612969525e431b190965457a0e3feb1fc1ea4
Andre Hadianto <andrehadiantolesmana@gmail.com>
2021-02-09 18:16:05 +0800

    refactor: change description alert to Chargeable

--- src/components/CustomerQuota/ItemsSelection/ItemsSelectionCard.tsx
+++ src/components/CustomerQuota/ItemsSelection/ItemsSelectionCard.tsx
@@ -78,7 +78,7 @@ export const ItemsSelectionCard: FunctionComponent<ItemsSelectionCard> = ({
     (product) => product.categoryType === "APPEAL"
   );
   const isChargeable = cart.some(
-    (cartItem) => cartItem.descriptionAlert === "*chargeable"
+    (cartItem) => cartItem.descriptionAlert === "Chargeable"
   );
   return (
     <View>
