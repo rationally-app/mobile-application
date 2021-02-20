3eafe5bf609b22ad88e0533d01aeef3cf3b57f28
Andre Hadianto <andrehadiantolesmana@gmail.com>
2021-02-20 10:07:35 +0800

    fix: useTranslate to use callback and memo

--- src/hooks/useTranslate/useTranslate.ts
+++ src/hooks/useTranslate/useTranslate.ts
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useCallback, useContext, useMemo } from "react";
 import { Translations } from "../../common/i18n/translations/type";
 import { CampaignConfigContext } from "../../context/campaignConfig";
 import { CampaignPolicy } from "../../types";
@@ -44,27 +44,36 @@ export const useTranslate = (): TranslationHook => {
   const { c13n } = useContext(CampaignConfigContext);
   const { allCampaignConfigs } = useContext(CampaignConfigsStoreContext);
 
-  const c13nt = (
-    key: string,
-    campaignKey?: string,
-    defaultString?: string
-  ): string => {
-    let c13nStore = c13n;
-    if (campaignKey) {
-      c13nStore = allCampaignConfigs[campaignKey]?.c13n ?? null;
-    }
-    return (c13nStore && key && c13nStore[key]) ?? defaultString ?? key;
-  };
+  const c13nt = useCallback(
+    (key: string, campaignKey?: string, defaultString?: string): string => {
+      let c13nStore = c13n;
+      if (campaignKey) {
+        c13nStore = allCampaignConfigs[campaignKey]?.c13n ?? null;
+      }
+      return (c13nStore && key && c13nStore[key]) ?? defaultString ?? key;
+    },
+    [allCampaignConfigs, c13n]
+  );
 
-  const c13ntForUnit = (
-    unit: CampaignPolicy["quantity"]["unit"]
-  ): CampaignPolicy["quantity"]["unit"] =>
-    unit && {
-      ...unit,
-      label: c13nt(unit.label),
-    };
+  const c13ntForUnit = useCallback(
+    (
+      unit: CampaignPolicy["quantity"]["unit"]
+    ): CampaignPolicy["quantity"]["unit"] =>
+      unit && {
+        ...unit,
+        label: c13nt(unit.label),
+      },
+    [c13nt]
+  );
 
   const i18nt = i18ntWithValidator;
 
-  return { c13nt, c13ntForUnit, i18nt };
+  return useMemo(
+    () => ({
+      c13nt,
+      c13ntForUnit,
+      i18nt,
+    }),
+    [c13nt, c13ntForUnit, i18nt]
+  );
 };
