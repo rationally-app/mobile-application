aebd898eb4dfcc2eb6801bcef118049d981328d5
manikandan <maksha@outlook.com>
2021-03-19 10:43:25 +0800

    fix: updated with fontSize in style

--- storybook/stories/Layout/AppText.tsx
+++ storybook/stories/Layout/AppText.tsx
@@ -2,21 +2,21 @@ import React from "react";
 import { storiesOf } from "@storybook/react-native";
 import { AppText } from "../../../src/components/Layout/AppText";
 import { View } from "react-native";
-import { size, color } from "../../../src/common/styles";
+import { size, color, fontSize } from "../../../src/common/styles";
 
-const appTextElement: JSX.Element[] = [
+const appTextElements: JSX.Element[] = [
   <AppText key="0">Default app text</AppText>,
   <AppText key="1" style={{ color: color("red", 60) }}>
     Customized text in red-60
   </AppText>,
-  <AppText key="2" style={{ fontSize: size(3) }}>
+  <AppText key="2" style={{ fontSize: fontSize(3) }}>
     customized text in fontSize-3
   </AppText>,
 ];
 
 storiesOf("Layout", module).add("AppText", () => (
   <View style={{ margin: size(3) }}>
-    {appTextElement.map((appTextElement, index) => (
+    {appTextElements.map((appTextElement, index) => (
       <View key={index} style={{ marginBottom: size(1) }}>
         {appTextElement}
       </View>
