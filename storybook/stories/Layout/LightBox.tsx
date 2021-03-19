1dee974724148b5a7e7f3fcb0bd402b5fb88c705
manikandan <maksha@outlook.com>
2021-03-19 11:58:08 +0800

    chore: updated with apptext element

--- storybook/stories/Layout/LightBox.tsx
+++ storybook/stories/Layout/LightBox.tsx
@@ -1,14 +1,15 @@
 import React from "react";
 import { storiesOf } from "@storybook/react-native";
 import { LightBox } from "../../../src/components/Layout/LightBox";
-import { View, Text } from "react-native";
+import { AppText } from "../../../src/components/Layout/AppText";
+import { View } from "react-native";
 
 storiesOf("Layout", module).add("LightBox", () => (
   <View>
     <LightBox
       width={200}
       height={200}
-      label={<Text>Box size (200,200)</Text>}
+      label={<AppText>Box size (200,200)</AppText>}
     />
   </View>
 ));
