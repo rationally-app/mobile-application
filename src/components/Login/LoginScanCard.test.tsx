2be21eb06499c7ae5131b73238bb30a363bd29ce
Andre Hadianto <andrehadiantolesmana@gmail.com>
2021-03-19 15:19:15 +0800

    feat: implement loginscancard component testing

---
+++ src/components/Login/LoginScanCard.test.tsx
@@ -0,0 +1,27 @@
+import { render, fireEvent, cleanup } from "@testing-library/react-native";
+import React from "react";
+import { LoginScanCard } from "./LoginScanCard";
+import "../../common/i18n/i18nMock";
+
+const onToggleScanner = jest.fn();
+const isLoading = false;
+
+describe("LoginScanCard", () => {
+  afterEach(() => {
+    cleanup();
+    jest.resetAllMocks();
+  });
+
+  it("should be able to render without error", async () => {
+    expect.hasAssertions();
+
+    const { getByTestId } = render(
+      <LoginScanCard onToggleScanner={onToggleScanner} isLoading={isLoading} />
+    );
+
+    const scanButton = getByTestId("base-button");
+
+    fireEvent.press(scanButton);
+    expect(onToggleScanner).toHaveBeenCalledTimes(1);
+  });
+});
