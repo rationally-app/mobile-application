683968ecc2bd66012a02fed8c9705391a9a8db33
Immanuella Lim <immanuella_lim@tech.gov.sg>
2021-03-22 13:22:33 +0800

    refactor: use switch statement instead

--- src/components/CustomerQuota/ItemsSelection/IdentifierLayout/IdentifierScanModal.tsx
+++ src/components/CustomerQuota/ItemsSelection/IdentifierLayout/IdentifierScanModal.tsx
@@ -16,15 +16,21 @@ export const IdentifierScanModal: FunctionComponent<{
     }
   };
 
-  let barcodeType = BarCodeScanner.Constants.BarCodeType.qr;
-  if (type === "CODE_128")
-    barcodeType = BarCodeScanner.Constants.BarCodeType.code128;
-  else if (type === "CODE_39")
-    barcodeType = BarCodeScanner.Constants.BarCodeType.code39;
-  // the idea is to drop the BARCODE type after we have updated this to prod
-  // without creating breaking changes, hence we currently support both types
-  else if (type === "BARCODE")
-    barcodeType = BarCodeScanner.Constants.BarCodeType.code39;
+  let barcodeType;
+  switch (type) {
+    case "CODE_128":
+      barcodeType = BarCodeScanner.Constants.BarCodeType.code128;
+      break;
+    // the idea is to drop the BARCODE type after we have updated this to prod
+    // without creating breaking changes, hence we currently support both types
+    case "CODE_39":
+    case "BARCODE":
+      barcodeType = BarCodeScanner.Constants.BarCodeType.code39;
+      break;
+    default:
+      barcodeType = BarCodeScanner.Constants.BarCodeType.qr;
+      break;
+  }
 
   return (
     <Modal
