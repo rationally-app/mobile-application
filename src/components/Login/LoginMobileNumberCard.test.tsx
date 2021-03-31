6d7650be386ba9f26ffa9b07d8047c133df8eb59
Andre Hadianto <40631483+andrehadianto@users.noreply.github.com>
2021-03-31 10:38:13 +0800

    feat(tech debt): FE components testing (#325)
    * feat: implement a test helper function to wrap components to test with providers
    
    * feat: implement component test for LoginMobileNumberCard
    
    * refactor: more explanatory test description. add TODO
    
    * feat: implement component test for LoginOTPCard
    
    * fix: migrate to 7.2.0
    
    * fix: update assertions method.
    
    * chore: remove empty beforeEach
    
    * feat: update testing library of loginmobilenumbercard
    
    * feat: implement loginscancard component testing
    
    * feat: update loginOTPCard tests
    
    * chore: use variables, fix malformed test, better test description

---
+++ src/components/Login/LoginMobileNumberCard.test.tsx
@@ -0,0 +1,124 @@
+import {
+  render,
+  fireEvent,
+  cleanup,
+  waitFor,
+} from "@testing-library/react-native";
+import React from "react";
+import { Sentry } from "../../utils/errorTracking";
+import { AlertModalContextProvider } from "../../context/alert";
+import { CreateProvidersWrapper } from "../../test/helpers/providers";
+import { LoginMobileNumberCard } from "./LoginMobileNumberCard";
+import "../../common/i18n/i18nMock";
+
+jest.mock("../../utils/errorTracking");
+const mockCaptureException = jest.fn();
+(Sentry.captureException as jest.Mock).mockImplementation(mockCaptureException);
+
+const setLoginState = jest.fn();
+const setMobileNumber = jest.fn();
+const setCountryCode = jest.fn();
+const mockHandleRequestOTP = jest.fn().mockResolvedValue(true);
+
+const OTPInputId = "login-phone-number-input";
+const submitButtonId = "login-send-otp-button";
+const invalidMessage = "Enter a valid contact number.";
+
+describe("LoginMobileNumberCard", () => {
+  afterEach(() => {
+    cleanup();
+    jest.resetAllMocks();
+  });
+
+  it("should request for OTP to the phone number successfully on pressing submit", async () => {
+    expect.assertions(6);
+    const { getByTestId, queryByText } = render(
+      <LoginMobileNumberCard
+        setLoginStage={setLoginState}
+        setMobileNumber={setMobileNumber}
+        setCountryCode={setCountryCode}
+        handleRequestOTP={mockHandleRequestOTP}
+      />
+    );
+
+    const phoneNumberInput = getByTestId(OTPInputId);
+    const submitButton = getByTestId(submitButtonId);
+
+    fireEvent(phoneNumberInput, "onChangeText", "88888888");
+    expect(phoneNumberInput.props["value"]).toEqual("8888 8888");
+
+    fireEvent.press(submitButton);
+    expect(queryByText("Send OTP")).toBeNull();
+
+    await waitFor(() => {
+      expect(mockHandleRequestOTP).toHaveBeenCalledTimes(1);
+      expect(setMobileNumber).toHaveBeenCalledWith("88888888");
+      expect(setCountryCode).toHaveBeenCalledWith("+65");
+      expect(setLoginState).toHaveBeenCalledWith("OTP");
+    });
+  });
+
+  describe("should show error modal", () => {
+    it("when contact number phone number input is empty", async () => {
+      expect.assertions(6);
+      const { getByTestId, queryByText } = render(
+        <CreateProvidersWrapper
+          providers={[{ provider: AlertModalContextProvider }]}
+        >
+          <LoginMobileNumberCard
+            setLoginStage={setLoginState}
+            setMobileNumber={setMobileNumber}
+            setCountryCode={setCountryCode}
+            handleRequestOTP={mockHandleRequestOTP}
+          />
+        </CreateProvidersWrapper>
+      );
+      const phoneNumberInput = getByTestId(OTPInputId);
+      const submitButton = getByTestId(submitButtonId);
+
+      fireEvent(phoneNumberInput, "onChangeText", "");
+      expect(phoneNumberInput.props["value"]).toEqual("");
+
+      fireEvent.press(submitButton);
+      expect(queryByText(invalidMessage)).not.toBeNull();
+
+      await waitFor(() => {
+        expect(mockHandleRequestOTP).not.toHaveBeenCalled();
+        expect(setMobileNumber).not.toHaveBeenCalled();
+        expect(setCountryCode).not.toHaveBeenCalled();
+        expect(setLoginState).not.toHaveBeenCalled();
+      });
+    });
+
+    it("when contact number phone number input is invalid", async () => {
+      expect.assertions(6);
+      const { getByTestId, queryByText } = render(
+        <CreateProvidersWrapper
+          providers={[{ provider: AlertModalContextProvider }]}
+        >
+          <LoginMobileNumberCard
+            setLoginStage={setLoginState}
+            setMobileNumber={setMobileNumber}
+            setCountryCode={setCountryCode}
+            handleRequestOTP={mockHandleRequestOTP}
+          />
+        </CreateProvidersWrapper>
+      );
+      const phoneNumberInput = getByTestId(OTPInputId);
+      const submitButton = getByTestId(submitButtonId);
+
+      fireEvent(phoneNumberInput, "onChangeText", "888888888");
+      expect(phoneNumberInput.props["value"]).toEqual("888888888");
+
+      fireEvent.press(submitButton);
+      expect(queryByText(invalidMessage)).not.toBeNull();
+
+      await waitFor(() => {
+        expect(mockHandleRequestOTP).not.toHaveBeenCalled();
+        expect(setMobileNumber).not.toHaveBeenCalled();
+        expect(setCountryCode).not.toHaveBeenCalled();
+        expect(setLoginState).not.toHaveBeenCalled();
+      });
+    });
+  });
+});
---
+++ src/test/helpers/providers.tsx
@@ -0,0 +1,38 @@
+import React, { FunctionComponent, ComponentType, ReactElement } from "react";
+
+interface Providers {
+  providers: {
+    provider: ComponentType | FunctionComponent;
+    props?: Record<any, any>;
+  }[];
+}
+
+/**
+ * Test helper function to dynamically wrap the tested components with the required context providers.
+ * The order of the context providers is important!
+ *
+ * Current order used based on src/navigation/index:
+ * - ErrorBoundary
+ * - FontLoader
+ * - ConfigContextProvider
+ * - AuthStoreContextProvider
+ * - CampaignConfigsStoreContextProvider
+ * - HelpModalContextProvider
+ * - AlertModalContextProvider
+ * - ImportantMessageContextProvider
+ * - DrawerContextProvider
+ * - IdentificationContextProvider
+ *
+ * @param providers Provider component and its props if required
+ */
+export const CreateProvidersWrapper: FunctionComponent<Providers> = ({
+  providers,
+  children,
+}): ReactElement => (
+  <>
+    {providers.reduceRight((composed, newProvider) => {
+      const { provider: Provider, props } = newProvider;
+      return <Provider {...props}>{composed}</Provider>;
+    }, <>{children}</>)}
+  </>
+);
