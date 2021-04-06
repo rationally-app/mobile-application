b843b024df790c9f82171f90a7abe662b568a5aa
Andre Hadianto <andrehadiantolesmana@gmail.com>
2021-04-06 14:50:36 +0800

    chore: remove unnecessary mock

--- jest.setup.ts
+++ jest.setup.ts
@@ -2,15 +2,6 @@ import "react-native-gesture-handler/jestSetup";
 import "@testing-library/jest-native/extend-expect";
 import "isomorphic-fetch";
 
-jest.mock("@expo/vector-icons", () => ({
-  Feather: "Feather Icons",
-  Ionicons: "Ionicons Icons",
-  AntDesign: "AntDesign Icons",
-  FontAwesome: "FontAwesome Icons",
-}));
-
-jest.mock("expo-constants", () => ({ manifest: { revisionId: "BUILD NO" } }));
-jest.mock("expo-sqlite", () => ({ default: {} }));
 jest.mock(
   "react-native/Libraries/Components/Touchable/TouchableOpacity",
   () => "TouchableOpacity"
