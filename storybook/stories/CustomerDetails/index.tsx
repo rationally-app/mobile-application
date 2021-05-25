afc1017a0f101da32e86adb46391f0dea34b26dc
manikandan <maksha@outlook.com>
2021-05-25 16:38:34 +0800

    refactor: breakdown the customerDetails stroies

--- storybook/stories/CustomerDetails/index.tsx
+++ storybook/stories/CustomerDetails/index.tsx
@@ -1,13 +1,2 @@
-import React from "react";
-import { storiesOf } from "@storybook/react-native";
-import { View } from "react-native";
-import CollectCustomerDetailsScreen from "../../../src/navigation/CustomerQuotaStack/CollectCustomerDetailsScreen";
-import { mockReactNavigationDecorator } from "../mocks/navigation";
-
-storiesOf("Screen", module)
-  .addDecorator((Story: any) => mockReactNavigationDecorator(Story))
-  .add("CollectCustomerDetails", () => (
-    <View>
-      <CollectCustomerDetailsScreen />
-    </View>
-  ));
+import "./CustomerDetailsScreen";
+import "./InputsElements";
