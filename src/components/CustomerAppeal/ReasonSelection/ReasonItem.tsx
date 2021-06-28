5924c1b70aff1cfd1b0014737f3858682bf87b57
Andre Hadianto <andrehadiantolesmana@gmail.com>
2021-06-28 11:08:53 +0800

    refactor: reasonItem description alert

--- src/components/CustomerAppeal/ReasonSelection/ReasonItem.tsx
+++ src/components/CustomerAppeal/ReasonSelection/ReasonItem.tsx
@@ -47,11 +47,9 @@ export const ReasonItem: FunctionComponent<{
     >
       <View style={styles.reasonLayout}>
         <AppText>{c13nt(description)}</AppText>
-        <AppText style={styles.reasonAlert}>
-          {descriptionAlert
-            ? c13nt(descriptionAlert, undefined, descriptionAlert)
-            : ""}
-        </AppText>
+        <AppText style={styles.reasonAlert}>{`${c13nt(
+          descriptionAlert
+        )}`}</AppText>
       </View>
     </TouchableOpacity>
   );
