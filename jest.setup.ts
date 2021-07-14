534881770eb7c15463232f7cfd94ce450e17ecab
Andre Hadianto <andrehadiantolesmana@gmail.com>
2021-07-14 14:20:22 +0800

    chore: rebase conflict

--- jest.setup.ts
+++ jest.setup.ts
@@ -2,6 +2,7 @@ import "@testing-library/jest-native/extend-expect";
 import "isomorphic-fetch";
 import mockAsyncStorage from "@react-native-async-storage/async-storage/jest/async-storage-mock";
 
+jest.mock("@react-native-async-storage/async-storage", () => mockAsyncStorage);
 jest.mock("react-native/Libraries/Vibration/Vibration", () => ({
   vibrate: () => "Vibration.vibrate",
 }));
