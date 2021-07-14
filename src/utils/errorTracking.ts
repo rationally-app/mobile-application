8d618844e2bfc4d495e94bc35930017908f396c4
Andre Hadianto <andrehadiantolesmana@gmail.com>
2021-07-14 16:43:38 +0800

    chore: update Sentry from PR comment

--- src/utils/errorTracking.ts
+++ src/utils/errorTracking.ts
@@ -3,34 +3,30 @@ import * as SentryExpo from "sentry-expo";
 const SENTRY_LOG_PREFIX = "[dev-sentry]";
 
 // This dev mock only implements the 4 currently used Sentry functions
-let Sentry: Pick<typeof SentryExpo, "init"> & {
-  Browser: Pick<
-    typeof SentryExpo.Browser,
-    "addBreadcrumb" | "captureException"
+let Sentry: Pick<typeof SentryExpo, "init"> &
+  Pick<
+    typeof SentryExpo.Native,
+    "setRelease" | "addBreadcrumb" | "captureException"
   >;
-  Native: Pick<typeof SentryExpo.Native, "setRelease">;
-};
-
 if (__DEV__) {
   Sentry = {
     init: (): void => console.log(`${SENTRY_LOG_PREFIX} initialised`),
-    Native: {
-      setRelease: (release: string): void =>
-        console.log(`${SENTRY_LOG_PREFIX} setRelease: ${release}`),
-    },
-    Browser: {
-      addBreadcrumb: (breadcrumb: unknown): void =>
-        console.log(
-          `${SENTRY_LOG_PREFIX} addBreadcrumb: ${JSON.stringify(breadcrumb)}`
-        ),
-      captureException: (e: unknown): string => {
-        console.warn(`${SENTRY_LOG_PREFIX} captureException: ${e}`);
-        return "eventId";
-      },
+    setRelease: (release: string): void =>
+      console.log(`${SENTRY_LOG_PREFIX} setRelease: ${release}`),
+    addBreadcrumb: (breadcrumb: unknown): void =>
+      console.log(
+        `${SENTRY_LOG_PREFIX} addBreadcrumb: ${JSON.stringify(breadcrumb)}`
+      ),
+    captureException: (e: unknown): string => {
+      console.warn(`${SENTRY_LOG_PREFIX} captureException: ${e}`);
+      return "eventId";
     },
   };
 } else {
-  Sentry = SentryExpo;
+  Sentry = {
+    ...SentryExpo.Native,
+    init: SentryExpo.init,
+  };
 }
 
 export { Sentry };
