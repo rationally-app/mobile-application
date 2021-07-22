3aa8eeced9612fc5a6dc35b3ee87b10d97a75ca4
Jedidiah Koh <jedidiahkkh@gmail.com>
2021-07-22 18:18:54 +0800

    feat: remove v1 storage method for authStore
    May be considered a breaking change for anyone who has not updated the
    app in 9 months.

--- src/context/authStore.tsx
+++ src/context/authStore.tsx
@@ -19,15 +19,6 @@ export const AUTH_CREDENTIALS_STORE_KEY = "AUTH_STORE";
 /** Number of credentials per bucket. Calculated with 2048/286. 286 is the length of a stringified AuthCredentials object. */
 export const BUCKET_SIZE = 7;
 
-/** @deprecated */
-export const SESSION_TOKEN_KEY = "SESSION_TOKEN";
-
-/** @deprecated */
-export const EXPIRY_KEY = "EXPIRY_KEY";
-
-/** @deprecated */
-export const ENDPOINT_KEY = "ENDPOINT_KEY";
-
 type AuthCredentialsMap = {
   [key: string]: AuthCredentials;
 };
@@ -49,7 +40,8 @@ export const AuthStoreContext = createContext<AuthStoreContext>({
 });
 
 /**
- * Uses three versions of auth credential storage. Will automatically migrate to v3 and clear the values in v1 and v2 storage
+ * Uses three versions of auth credential storage. Will automatically migrate to v3 and clear the values in v2 storage
+ * Assumes that no one uses v1 storage anymore as of Jul 2021.
  *
  * Versions:
  *
@@ -159,36 +151,6 @@ export const AuthStoreContextProvider: FunctionComponent<{
         await AsyncStorage.removeItem(AUTH_CREDENTIALS_STORE_KEY);
       }
 
-      // check v1 storage
-      if (!hasUpdatedData) {
-        const values = await AsyncStorage.multiGet([
-          SESSION_TOKEN_KEY,
-          EXPIRY_KEY,
-          ENDPOINT_KEY,
-        ]);
-        const [sessionToken, expiry, endpoint] = values.map(
-          (value) => value[1]
-        );
-        if (sessionToken && endpoint && expiry !== undefined) {
-          const newAuthCredentials = {
-            [endpoint]: {
-              operatorToken: "",
-              sessionToken,
-              endpoint,
-              expiry: Number(expiry),
-            },
-          };
-          setAuthCredentialsMap(newAuthCredentials);
-          hasUpdatedData = true;
-        }
-      }
-      if (hasUpdatedData) {
-        await AsyncStorage.multiRemove([
-          SESSION_TOKEN_KEY,
-          EXPIRY_KEY,
-          ENDPOINT_KEY,
-        ]);
-      }
       return hasUpdatedData;
     };
 
