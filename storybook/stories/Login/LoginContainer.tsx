25f3479da0401c682306bfa1e0707d681f001590
maksha19 <66373739+maksha19@users.noreply.github.com>
2021-07-23 09:22:17 +0800

    feat: add login screen storybook (#400)
    Co-authored-by: manikandan <maksha@outlook.com>

---
+++ storybook/stories/Login/LoginContainer.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { storiesOf } from "@storybook/react-native";
+import { View } from "react-native";
+import { InitialisationContainer } from "../../../src/components/Login/LoginContainer";
+import { navigation } from "../mocks/navigation";
+
+storiesOf("Screen", module).add("LoginScreen", () => (
+  <View style={{ height: "100%" }}>
+    <InitialisationContainer navigation={navigation} />
+  </View>
+));
