dc9f3a62ecc6127de22a9dd217efce03ffc34bd3
Jedidiah Koh <jedidiahkkh@gmail.com>
2021-08-24 16:00:24 +0800

    test: refactor logoutScreen tests to be neater

--- src/components/Logout/LogoutScreen.test.tsx
+++ src/components/Logout/LogoutScreen.test.tsx
@@ -1,5 +1,5 @@
 import { render, waitFor } from "@testing-library/react-native";
-import React, { FunctionComponent } from "react";
+import React, { ContextType, FunctionComponent } from "react";
 import { NavigationScreenProp, withNavigation } from "react-navigation";
 import { AlertModalContext } from "../../context/alert";
 import { AuthStoreContext } from "../../context/authStore";
@@ -39,57 +39,75 @@ const mockCallLogout = callLogout as jest.MockedFunction<typeof callLogout>;
 
 const LogoutScreenContainer = withNavigation(LogoutScreen);
 
-const keyA = "keyA";
-const keyB = "keyB";
-const keyC = "keyC";
-
-const authA: AuthCredentials = {
-  endpoint: "endA",
-  expiry: 0,
-  operatorToken: "optA",
-  sessionToken: "sesA",
-};
-const authB: AuthCredentials = {
-  endpoint: "endB",
-  expiry: 0,
-  operatorToken: "optB",
-  sessionToken: "sesB",
-};
-const authC: AuthCredentials = {
-  endpoint: "endC",
-  expiry: 0,
-  operatorToken: "optC",
-  sessionToken: "sesC",
-};
-
-const authCredentials = {
-  [keyA]: authA,
-  [keyB]: authB,
-  [keyC]: authC,
-};
-
-const defaultAuthStoreContextValue = {
-  authCredentials: authCredentials,
-  clearAuthCredentials: jest.fn(),
-  hasLoadedFromStore: true,
-  removeAuthCredentials: jest.fn(),
-  setAuthCredentials: jest.fn(),
-};
-
-const defaultCampaignConfigStoreContextValue = {
-  allCampaignConfigs: {},
-  clearCampaignConfigs: jest.fn(),
-  hasLoadedFromStore: true,
-  removeCampaignConfig: jest.fn(),
-  setCampaignConfig: jest.fn(),
-};
-
-const defaultConfigContextProviderValue = {
-  config: { appMode: AppMode.production },
-  setConfigValue: jest.fn(),
-};
-
 describe("LogoutScreen", () => {
+  let keyA: string;
+  let keyB: string;
+  let keyC: string;
+
+  let authA: AuthCredentials;
+  let authB: AuthCredentials;
+  let authC: AuthCredentials;
+
+  let authCredentials: { [key: string]: AuthCredentials };
+
+  let defaultAuthStoreContextValue: ContextType<typeof AuthStoreContext>;
+  let defaultCampaignConfigStoreContextValue: ContextType<
+    typeof CampaignConfigsStoreContext
+  >;
+  let defaultConfigContextProviderValue: ContextType<typeof ConfigContext>;
+
+  beforeAll(() => {
+    keyA = "keyA";
+    keyB = "keyB";
+    keyC = "keyC";
+
+    authA = {
+      endpoint: "endA",
+      expiry: 0,
+      operatorToken: "optA",
+      sessionToken: "sesA",
+    };
+    authB = {
+      endpoint: "endB",
+      expiry: 0,
+      operatorToken: "optB",
+      sessionToken: "sesB",
+    };
+    authC = {
+      endpoint: "endC",
+      expiry: 0,
+      operatorToken: "optC",
+      sessionToken: "sesC",
+    };
+
+    authCredentials = {
+      [keyA]: authA,
+      [keyB]: authB,
+      [keyC]: authC,
+    };
+
+    defaultAuthStoreContextValue = {
+      authCredentials: authCredentials,
+      clearAuthCredentials: jest.fn(),
+      hasLoadedFromStore: true,
+      removeAuthCredentials: jest.fn(),
+      setAuthCredentials: jest.fn(),
+    };
+
+    defaultCampaignConfigStoreContextValue = {
+      allCampaignConfigs: {},
+      clearCampaignConfigs: jest.fn(),
+      hasLoadedFromStore: true,
+      removeCampaignConfig: jest.fn(),
+      setCampaignConfig: jest.fn(),
+    };
+
+    defaultConfigContextProviderValue = {
+      config: { appMode: AppMode.production },
+      setConfigValue: jest.fn(),
+    };
+  });
+
   beforeEach(() => {
     mockCaptureException.mockReset().mockName("captureException");
     mockNavigate.mockReset().mockName("navigate");
@@ -121,161 +139,103 @@ describe("LogoutScreen", () => {
     expect(mockCallLogout).toHaveBeenCalledWith("sesC", "optC", "endC");
   });
 
-  it("should logout from campaigns with no error and vice versa", async () => {
-    expect.assertions(9);
+  describe("when all campaigns logout successfully", () => {
+    it("should clear all credentials and configs and move to login screen", async () => {
+      expect.assertions(4);
 
-    mockCallLogout.mockImplementation(async (sessionToken) => {
-      if (sessionToken === "sesA") throw new LogoutError("");
-    });
-    const mockRemoveAuthCredentials = jest
-      .fn()
-      .mockName("removeAuthCredentials");
-    const mockRemoveCampaignConfig = jest.fn().mockName("removeCampaignConfig");
+      const mockClearAuthCredentials = jest.fn();
+      const mockClearCampaignConfigs = jest.fn();
 
-    render(
-      <AuthStoreContext.Provider
-        value={{
-          ...defaultAuthStoreContextValue,
-          removeAuthCredentials: mockRemoveAuthCredentials,
-        }}
-      >
-        <CampaignConfigsStoreContext.Provider
+      render(
+        <AuthStoreContext.Provider
           value={{
-            ...defaultCampaignConfigStoreContextValue,
-            removeCampaignConfig: mockRemoveCampaignConfig,
+            ...defaultAuthStoreContextValue,
+            clearAuthCredentials: mockClearAuthCredentials,
           }}
         >
-          <LogoutScreenContainer />
-        </CampaignConfigsStoreContext.Provider>
-      </AuthStoreContext.Provider>
-    );
-
-    await waitFor(() => expect(mockNavigate).toHaveBeenCalledTimes(1));
-
-    expect(mockRemoveAuthCredentials).toHaveBeenCalledTimes(2);
-    expect(mockRemoveAuthCredentials).not.toHaveBeenCalledWith(keyA);
-    expect(mockRemoveAuthCredentials).toHaveBeenCalledWith(keyB);
-    expect(mockRemoveAuthCredentials).toHaveBeenCalledWith(keyC);
-
-    expect(mockRemoveCampaignConfig).toHaveBeenCalledTimes(2);
-    expect(mockRemoveCampaignConfig).not.toHaveBeenCalledWith(keyA);
-    expect(mockRemoveCampaignConfig).toHaveBeenCalledWith(keyB);
-    expect(mockRemoveCampaignConfig).toHaveBeenCalledWith(keyC);
-  });
-
-  it("should clear all credentials and configs and move to login screen upon successful logout", async () => {
-    expect.assertions(4);
-
-    const mockClearAuthCredentials = jest.fn();
-    const mockClearCampaignConfigs = jest.fn();
-
-    render(
-      <AuthStoreContext.Provider
-        value={{
-          ...defaultAuthStoreContextValue,
-          clearAuthCredentials: mockClearAuthCredentials,
-        }}
-      >
-        <CampaignConfigsStoreContext.Provider
-          value={{
-            ...defaultCampaignConfigStoreContextValue,
-            clearCampaignConfigs: mockClearCampaignConfigs,
-          }}
-        >
-          <LogoutScreenContainer />
-        </CampaignConfigsStoreContext.Provider>
-      </AuthStoreContext.Provider>
-    );
-
-    await waitFor(() => expect(mockNavigate).toHaveBeenCalledTimes(1));
-
-    expect(mockNavigate).toHaveBeenCalledWith("LoginScreen");
-    expect(mockClearAuthCredentials).toHaveBeenCalledTimes(1);
-    expect(mockClearCampaignConfigs).toHaveBeenCalledTimes(1);
-  });
-
-  it("should not clear all data and should move to campaign selection screen upon unsuccessful logout", async () => {
-    expect.assertions(4);
-
-    mockCallLogout.mockRejectedValue(new LogoutError(""));
-
-    const mockClearAuthCredentials = jest.fn();
-    const mockClearCampaignConfigs = jest.fn();
-
-    render(
-      <AuthStoreContext.Provider
-        value={{
-          ...defaultAuthStoreContextValue,
-          clearAuthCredentials: mockClearAuthCredentials,
-        }}
-      >
-        <CampaignConfigsStoreContext.Provider
-          value={{
-            ...defaultCampaignConfigStoreContextValue,
-            clearCampaignConfigs: mockClearCampaignConfigs,
-          }}
-        >
-          <LogoutScreenContainer />
-        </CampaignConfigsStoreContext.Provider>
-      </AuthStoreContext.Provider>
-    );
-
-    await waitFor(() => expect(mockNavigate).toHaveBeenCalledTimes(1));
-
-    expect(mockNavigate).toHaveBeenCalledWith("CampaignLocationsScreen", {
-      shouldAutoLoad: false,
-    });
-    expect(mockClearAuthCredentials).not.toHaveBeenCalled();
-    expect(mockClearCampaignConfigs).not.toHaveBeenCalled();
-  });
-
-  it("should display an alert if an LogoutError was thrown", async () => {
-    expect.assertions(3);
-
-    mockCallLogout.mockRejectedValue(new LogoutError(""));
-    const showErrorAlert = jest.fn();
-
-    render(
-      <AlertModalContext.Provider
-        value={{
-          clearAlert: jest.fn(),
-          showConfirmationAlert: jest.fn(),
-          showErrorAlert,
-          showWarnAlert: jest.fn(),
-        }}
-      >
-        <AuthStoreContext.Provider value={defaultAuthStoreContextValue}>
           <CampaignConfigsStoreContext.Provider
-            value={defaultCampaignConfigStoreContextValue}
+            value={{
+              ...defaultCampaignConfigStoreContextValue,
+              clearCampaignConfigs: mockClearCampaignConfigs,
+            }}
           >
             <LogoutScreenContainer />
           </CampaignConfigsStoreContext.Provider>
         </AuthStoreContext.Provider>
-      </AlertModalContext.Provider>
-    );
+      );
 
-    await waitFor(() => expect(mockNavigate).toHaveBeenCalledTimes(1));
+      await waitFor(() => expect(mockNavigate).toHaveBeenCalledTimes(1));
 
-    expect(showErrorAlert).toHaveBeenCalledTimes(1);
-    expect(showErrorAlert).toHaveBeenCalledWith(new LogoutError(""));
+      expect(mockNavigate).toHaveBeenCalledWith("LoginScreen");
+      expect(mockClearAuthCredentials).toHaveBeenCalledTimes(1);
+      expect(mockClearCampaignConfigs).toHaveBeenCalledTimes(1);
+    });
   });
 
-  it("should let error boundary handle if NetworkError thrown", async () => {
-    expect.assertions(4);
+  describe("when there are errors logging out some campaigns", () => {
+    it("should only logout from campaigns with no error and not clear all credentials", async () => {
+      expect.assertions(11);
 
-    mockCallLogout.mockRejectedValueOnce(new NetworkError(""));
-    const showErrorAlert = jest.fn();
+      mockCallLogout.mockImplementation(async (sessionToken) => {
+        if (sessionToken === "sesA") throw new LogoutError("");
+      });
+      const mockRemoveAuthCredentials = jest
+        .fn()
+        .mockName("removeAuthCredentials");
+      const mockRemoveCampaignConfig = jest
+        .fn()
+        .mockName("removeCampaignConfig");
 
-    render(
-      <ErrorBoundary>
-        <AlertModalContext.Provider
+      const mockClearAuthCredentials = jest
+        .fn()
+        .mockName("clearAuthCredentials");
+      const mockClearCampaignConfigs = jest
+        .fn()
+        .mockName("clearCampaignConfigs");
+
+      render(
+        <AuthStoreContext.Provider
           value={{
-            clearAlert: jest.fn(),
-            showConfirmationAlert: jest.fn(),
-            showErrorAlert,
-            showWarnAlert: jest.fn(),
+            ...defaultAuthStoreContextValue,
+            removeAuthCredentials: mockRemoveAuthCredentials,
+            clearAuthCredentials: mockClearAuthCredentials,
           }}
         >
+          <CampaignConfigsStoreContext.Provider
+            value={{
+              ...defaultCampaignConfigStoreContextValue,
+              removeCampaignConfig: mockRemoveCampaignConfig,
+              clearCampaignConfigs: mockClearCampaignConfigs,
+            }}
+          >
+            <LogoutScreenContainer />
+          </CampaignConfigsStoreContext.Provider>
+        </AuthStoreContext.Provider>
+      );
+
+      await waitFor(() => expect(mockNavigate).toHaveBeenCalledTimes(1));
+
+      expect(mockRemoveAuthCredentials).toHaveBeenCalledTimes(2);
+      expect(mockRemoveAuthCredentials).not.toHaveBeenCalledWith(keyA);
+      expect(mockRemoveAuthCredentials).toHaveBeenCalledWith(keyB);
+      expect(mockRemoveAuthCredentials).toHaveBeenCalledWith(keyC);
+
+      expect(mockRemoveCampaignConfig).toHaveBeenCalledTimes(2);
+      expect(mockRemoveCampaignConfig).not.toHaveBeenCalledWith(keyA);
+      expect(mockRemoveCampaignConfig).toHaveBeenCalledWith(keyB);
+      expect(mockRemoveCampaignConfig).toHaveBeenCalledWith(keyC);
+
+      expect(mockClearAuthCredentials).not.toHaveBeenCalled();
+      expect(mockClearCampaignConfigs).not.toHaveBeenCalled();
+    });
+
+    describe("when LogoutError is thrown", () => {
+      it("should move to campaign selection screen", async () => {
+        expect.assertions(2);
+
+        mockCallLogout.mockRejectedValue(new LogoutError(""));
+
+        render(
           <AuthStoreContext.Provider value={defaultAuthStoreContextValue}>
             <CampaignConfigsStoreContext.Provider
               value={defaultCampaignConfigStoreContextValue}
@@ -283,16 +243,85 @@ describe("LogoutScreen", () => {
               <LogoutScreenContainer />
             </CampaignConfigsStoreContext.Provider>
           </AuthStoreContext.Provider>
-        </AlertModalContext.Provider>
-      </ErrorBoundary>
-    );
+        );
 
-    // called twice, one in logout screen when capturing all exceptions and one
-    // at ErrorBoundary
-    await waitFor(() => expect(mockCaptureException).toHaveBeenCalledTimes(2));
-    expect(mockCaptureException).toHaveBeenCalledWith(new NetworkError(""));
+        await waitFor(() => expect(mockNavigate).toHaveBeenCalledTimes(1));
 
-    expect(showErrorAlert).not.toHaveBeenCalled();
-    expect(mockNavigate).not.toHaveBeenCalled();
+        expect(mockNavigate).toHaveBeenCalledWith("CampaignLocationsScreen", {
+          shouldAutoLoad: false,
+        });
+      });
+
+      it("should display an alert modal", async () => {
+        expect.assertions(3);
+
+        mockCallLogout.mockRejectedValue(new LogoutError(""));
+        const showErrorAlert = jest.fn();
+
+        render(
+          <AlertModalContext.Provider
+            value={{
+              clearAlert: jest.fn(),
+              showConfirmationAlert: jest.fn(),
+              showErrorAlert,
+              showWarnAlert: jest.fn(),
+            }}
+          >
+            <AuthStoreContext.Provider value={defaultAuthStoreContextValue}>
+              <CampaignConfigsStoreContext.Provider
+                value={defaultCampaignConfigStoreContextValue}
+              >
+                <LogoutScreenContainer />
+              </CampaignConfigsStoreContext.Provider>
+            </AuthStoreContext.Provider>
+          </AlertModalContext.Provider>
+        );
+
+        await waitFor(() => expect(mockNavigate).toHaveBeenCalledTimes(1));
+
+        expect(showErrorAlert).toHaveBeenCalledTimes(1);
+        expect(showErrorAlert).toHaveBeenCalledWith(new LogoutError(""));
+      });
+    });
+
+    describe("when other errors are thrown", () => {
+      it("should let error boundary handle", async () => {
+        expect.assertions(4);
+
+        mockCallLogout.mockRejectedValueOnce(new NetworkError(""));
+        const showErrorAlert = jest.fn();
+
+        render(
+          <ErrorBoundary>
+            <AlertModalContext.Provider
+              value={{
+                clearAlert: jest.fn(),
+                showConfirmationAlert: jest.fn(),
+                showErrorAlert,
+                showWarnAlert: jest.fn(),
+              }}
+            >
+              <AuthStoreContext.Provider value={defaultAuthStoreContextValue}>
+                <CampaignConfigsStoreContext.Provider
+                  value={defaultCampaignConfigStoreContextValue}
+                >
+                  <LogoutScreenContainer />
+                </CampaignConfigsStoreContext.Provider>
+              </AuthStoreContext.Provider>
+            </AlertModalContext.Provider>
+          </ErrorBoundary>
+        );
+
+        // called twice, one in logout screen when capturing all exceptions and one
+        // at ErrorBoundary
+        await waitFor(() =>
+          expect(mockCaptureException).toHaveBeenCalledTimes(2)
+        );
+        expect(mockCaptureException).toHaveBeenCalledWith(new NetworkError(""));
+
+        expect(showErrorAlert).not.toHaveBeenCalled();
+        expect(mockNavigate).not.toHaveBeenCalled();
+      });
+    });
   });
 });
