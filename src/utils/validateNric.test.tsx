5cfcc7f38e25adc3988052f988b58dbb666634fd
Andre Hadianto <40631483+andrehadianto@users.noreply.github.com>
2021-12-30 15:06:32 +0800

    feat: nric validation (#518)
    * feat: nric validation
    
    * chore: remove .npmrc
    
    * Revert "chore: remove .npmrc"
    
    This reverts commit ee8aaaa155c8656788c186336fd7ea28b51b11ed.
    
    * build: combine set npm to install packages
    
    * fix: build typo
    
    * Revert "fix: build typo"
    
    This reverts commit f661d1d6f814f238ce83c87d97d460a544175412.
    
    * fix: build typo
    
    * revert: build
    
    * fix: test
    
    * chore: test remove dev env
    
    * Revert "chore: test remove dev env"
    
    This reverts commit c7dbf0d97e81d651af18b4bc82ecf6ea2706c5bb.
    
    * chore: revert state
    
    * chore: bump to newer version because the old one was deleted
    
    * chore: add unit tests
    
    Co-authored-by: Ryan Goh <ryangohjq@gmail.com>

--- src/utils/validateNric.test.tsx
+++ src/utils/validateNric.test.tsx
@@ -1,23 +1,54 @@
 import { validateAndCleanNric } from "./validateNric";
 
 describe("validateAndCleanNric", () => {
-  it("should return cleaned nric for valid nric number", () => {
-    expect.assertions(10);
-    // https://en.wikipedia.org/wiki/National_Registration_Identity_Card
-    expect(validateAndCleanNric("S0000001I")).toBe("S0000001I");
-    expect(validateAndCleanNric("S0000002G")).toBe("S0000002G");
-    expect(validateAndCleanNric("S0000003E")).toBe("S0000003E");
-    expect(validateAndCleanNric("S0000004C")).toBe("S0000004C");
-    expect(validateAndCleanNric("  S0000004C")).toBe("S0000004C");
-    expect(validateAndCleanNric("  S0000004C    ")).toBe("S0000004C");
-    expect(validateAndCleanNric("G1234567X")).toBe("G1234567X");
-    expect(validateAndCleanNric("G1234567X150319SVP")).toBe("G1234567X");
-    expect(validateAndCleanNric("S0000001I123A")).toBe("S0000001I");
-    expect(validateAndCleanNric("S0000001I^&*(asd123")).toBe("S0000001I");
+  describe("should return cleaned nric for valid nric number", () => {
+    it("for S series", () => {
+      expect.assertions(4);
+      // https://en.wikipedia.org/wiki/National_Registration_Identity_Card
+      expect(validateAndCleanNric("S0000001I")).toBe("S0000001I");
+      expect(validateAndCleanNric("S0000002G")).toBe("S0000002G");
+      expect(validateAndCleanNric("S0000003E")).toBe("S0000003E");
+      expect(validateAndCleanNric("S0000004C")).toBe("S0000004C");
+    });
+
+    it("for T series", () => {
+      expect.assertions(2);
+      expect(validateAndCleanNric("T1928347F")).toBe("T1928347F");
+      expect(validateAndCleanNric("T7189231F")).toBe("T7189231F");
+    });
+
+    it("for F series", () => {
+      expect.assertions(2);
+      expect(validateAndCleanNric("F2143781Q")).toBe("F2143781Q");
+      expect(validateAndCleanNric("F8761312R")).toBe("F8761312R");
+    });
+
+    it("for G series", () => {
+      expect.assertions(2);
+      expect(validateAndCleanNric("G1234567X")).toBe("G1234567X");
+      expect(validateAndCleanNric("G1206026U")).toBe("G1206026U");
+    });
+
+    it("for M series", () => {
+      expect.assertions(4);
+      expect(validateAndCleanNric("M1234567K")).toBe("M1234567K");
+      expect(validateAndCleanNric("M0000002N")).toBe("M0000002N");
+      expect(validateAndCleanNric("M0000003L")).toBe("M0000003L");
+      expect(validateAndCleanNric("M0000004X")).toBe("M0000004X");
+    });
+
+    it("for unique cases", () => {
+      expect.assertions(5);
+      expect(validateAndCleanNric("  S0000004C")).toBe("S0000004C");
+      expect(validateAndCleanNric("  S0000004C    ")).toBe("S0000004C");
+      expect(validateAndCleanNric("G1234567X150319SVP")).toBe("G1234567X");
+      expect(validateAndCleanNric("S0000001I123A")).toBe("S0000001I");
+      expect(validateAndCleanNric("S0000001I^&*(asd123")).toBe("S0000001I");
+    });
   });
 
   it("should throw error for invalid NRIC", () => {
-    expect.assertions(7);
+    expect.assertions(16);
     const errorMessage = "Enter or scan a valid ID number.";
     expect(() => validateAndCleanNric("S0000001A")).toThrow(errorMessage);
     expect(() => validateAndCleanNric("123AS0000001I")).toThrow(errorMessage);
@@ -25,6 +56,15 @@ describe("validateAndCleanNric", () => {
     expect(() => validateAndCleanNric("S0000002B")).toThrow(errorMessage);
     expect(() => validateAndCleanNric("S0000003C")).toThrow(errorMessage);
     expect(() => validateAndCleanNric("S0000004D")).toThrow(errorMessage);
+    expect(() => validateAndCleanNric("M1234873S")).toThrow(errorMessage);
+    expect(() => validateAndCleanNric("M4398273E")).toThrow(errorMessage);
+    expect(() => validateAndCleanNric("M2987349P")).toThrow(errorMessage);
+    expect(() => validateAndCleanNric("G1234873S")).toThrow(errorMessage);
+    expect(() => validateAndCleanNric("G4398273E")).toThrow(errorMessage);
+    expect(() => validateAndCleanNric("F2987349P")).toThrow(errorMessage);
+    expect(() => validateAndCleanNric("F2128749P")).toThrow(errorMessage);
+    expect(() => validateAndCleanNric("T8728729U")).toThrow(errorMessage);
+    expect(() => validateAndCleanNric("T2916239R")).toThrow(errorMessage);
     expect(() => validateAndCleanNric("2821462")).toThrow(errorMessage);
   });
 });
--- src/utils/validateNric.tsx
+++ src/utils/validateNric.tsx
@@ -1,54 +1,5 @@
-// Thanks to https://gist.github.com/kyrene-chew/6f275325335ab27895beb7a9a7b4c1cb
+import { validate } from "@rationally-app/nric-validator";
 import { ERROR_MESSAGE } from "../context/alert";
-
-const validate = (nricInput: string): boolean => {
-  // validation rules
-  const nricRegex = /(\D)(\d{7})(\D)/;
-  const nricTypeRegex = /S|T|F|G/;
-  const weightArr = [2, 7, 6, 5, 4, 3, 2];
-  const nricLetterST = ["J", "Z", "I", "H", "G", "F", "E", "D", "C", "B", "A"];
-  const nricLetterFG = ["X", "W", "U", "T", "R", "Q", "P", "N", "M", "L", "K"];
-
-  // set nric to all uppercase to remove case sensitivity
-  const nric = nricInput.toUpperCase();
-
-  // returns false if it false basic validation
-  if (!nricRegex.exec(nric)) {
-    return false;
-  }
-
-  // get nric type
-  const nricArr = nric.match(nricRegex);
-  if (!nricArr) return false;
-  const nricType = nricArr[1];
-
-  // returns false if it false basic validation
-  if (!nricTypeRegex.exec(nricType)) {
-    return false;
-  }
-
-  // multiply each digit in the nric number by it's weight in order
-  const nricDigitsArr = nricArr[2].split("");
-  let total = 0;
-  for (let i = 0; i < nricDigitsArr.length; i++) {
-    total += parseInt(nricDigitsArr[i]) * weightArr[i];
-  }
-
-  // if the nric type is T or G, add 4 to the total
-  if (["T", "G"].indexOf(nricType) >= 0) {
-    total += 4;
-  }
-
-  // check last letter of nric for local
-  const letterIndex = total % 11;
-  const nricLetter = nricArr[3];
-  if (["S", "T"].indexOf(nricType) >= 0) {
-    return nricLetterST[letterIndex] === nricLetter;
-  }
-
-  // check last letter of nric for foreigners
-  return nricLetterFG[letterIndex] === nricLetter;
-};
 
 export const validateAndCleanNric = (inputNric: string): string => {
   const cleanedInputNric = inputNric.trim().slice(0, 9).toUpperCase();
