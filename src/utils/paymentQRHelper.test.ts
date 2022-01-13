a182525088cb2ff8c2330a9526aa757938f9b33f
Yong Jia Jie <yongjiajie.govtech@gmail.com>
2022-01-13 13:20:07 +0800

    feat(payment-qr-helper): add util updating transactions payment qr identifiers

--- src/utils/paymentQRHelper.test.ts
+++ src/utils/paymentQRHelper.test.ts
@@ -1,4 +1,8 @@
-import { findValueByKey } from "./paymentQRHelper";
+import { Transaction } from "../types";
+import {
+  findValueByKey,
+  updateTransactionsPaymentQRIdentifiers,
+} from "./paymentQRHelper";
 
 describe("tests for paymentQRHelper", () => {
   describe("tests for findValueByKey", () => {
@@ -34,5 +38,62 @@ describe("tests for paymentQRHelper", () => {
       expect(findValueByKey("nonExistentKey", records)).toBeUndefined();
     });
   });
+
+  describe("tests for updateTransactionsPaymentQRIdentifiers", () => {
+    let transactions: Array<Transaction>;
+
+    beforeEach(() => {
+      transactions = [
+        {
+          category: "category-a",
+          quantity: 1,
+          identifierInputs: [
+            {
+              label: "Payment QR",
+              scanButtonType: "QR",
+              validationRegex: "[\\s\\S]*",
+              value:
+                "00020101021126520008com.grab0136c99990dc-ab89-442f-accb-1fd96dd4eff627330015sg.com.dash.www0110000000901628460010com.myfave0128https://myfave.com/qr/6ntdz329360009SG.AIRPAY0119936009180000000468751810007SG.SGQR01121809112DE3C7020701.000703065743700402010503105060400000708202012105204581253037025802SG5920UDDERS UPPER THOMSON6009Singapore63047AEC",
+              textInputType: "PAYMENT_QR",
+            },
+            {
+              label: "merchantName",
+              isOptional: true,
+              value: "",
+              validationRegex: "^[a-zA-Z0-9 ]*$",
+              textInputType: "STRING",
+            },
+          ],
+        },
+      ];
+    });
+
+    it("should update identifiers properly", () => {
+      expect.assertions(1);
+      updateTransactionsPaymentQRIdentifiers(transactions);
+      expect(transactions).toStrictEqual([
+        {
+          category: "category-a",
+          quantity: 1,
+          identifierInputs: [
+            {
+              label: "Payment QR",
+              scanButtonType: "QR",
+              validationRegex: "[\\s\\S]*",
+              value:
+                "00020101021126520008com.grab0136c99990dc-ab89-442f-accb-1fd96dd4eff627330015sg.com.dash.www0110000000901628460010com.myfave0128https://myfave.com/qr/6ntdz329360009SG.AIRPAY0119936009180000000468751810007SG.SGQR01121809112DE3C7020701.000703065743700402010503105060400000708202012105204581253037025802SG5920UDDERS UPPER THOMSON6009Singapore63047AEC",
+              textInputType: "PAYMENT_QR",
+            },
+            {
+              label: "merchantName",
+              isOptional: true,
+              value: "UDDERS UPPER THOMSON",
+              validationRegex: "^[a-zA-Z0-9 ]*$",
+              textInputType: "STRING",
+            },
+          ],
+        },
+      ]);
+    });
   });
 });
--- src/utils/paymentQRHelper.ts
+++ src/utils/paymentQRHelper.ts
@@ -1,3 +1,6 @@
+import { parseAndValidateSGQR } from "@rationally-app/payment-qr-parser";
+import { Transaction } from "../types";
+
 /**
  * Returns the value of the first element in the provided record key.
  * If the provided record key does not exist, `undefined` is returned.
@@ -25,4 +28,45 @@ export const findValueByKey = (
   }
 
   return undefined;
+};
+
+/**
+ * Update transaction identifiers using parsed payment QR payload values.
+ *
+ * Each transaction's identifiers contain identifiers with `textInputType` `PAYMENT_QR`, which
+ * are payment QR payloads that shall be parsed.
+ *
+ * The parsed values are used to update identifiers where (1) `isOptional` is `true`, and (2)
+ * their `label` matches a key in the parsed payment QR payload. Only identifiers in the same
+ * transaction are updated.
+ *
+ * @param transactions Array of transaction data
+ */
+export const updateTransactionsPaymentQRIdentifiers = (
+  transactions: Array<Transaction>
+): void => {
+  transactions.forEach(({ identifierInputs }) => {
+    if (identifierInputs) {
+      const paymentQRPayload = identifierInputs.find(
+        ({ textInputType }) => textInputType === "PAYMENT_QR"
+      )?.value;
+
+      if (paymentQRPayload) {
+        // TODO: Expand support for payment QRs
+        // TODO: Perform preliminary parsing to determine payload type, then perform parsing
+        const paymentQRRecords = parseAndValidateSGQR(
+          paymentQRPayload
+        ) as Record<string, unknown>;
+
+        identifierInputs.forEach(
+          ({ textInputType, isOptional, label, value }, index) => {
+            if (textInputType !== "PAYMENT_QR" && isOptional) {
+              identifierInputs[index].value =
+                findValueByKey(label, paymentQRRecords) ?? value;
+            }
+          }
+        );
+      }
+    }
+  });
 };
