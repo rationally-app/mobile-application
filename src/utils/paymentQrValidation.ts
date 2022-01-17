701ae6909ac8bae7a9031280b4c6060697def8c9
Yong Jia Jie <yongjiajie.govtech@gmail.com>
2022-01-17 10:44:54 +0800

    docs: add TODO for paymentQrValidate

--- src/utils/paymentQrValidation.ts
+++ src/utils/paymentQrValidation.ts
@@ -2,6 +2,7 @@ import { parseAndValidateSGQR } from "@rationally-app/payment-qr-parser";
 
 const paymentQrValidate = (paymentQr: string): boolean => {
   try {
+    // TODO: Expand support for payment QRs
     parseAndValidateSGQR(paymentQr);
   } catch (e) {
     return false;
