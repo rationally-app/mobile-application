1aff2ff755e0ba3455c50ce1fa13c1a01f527891
Yong Jia Jie <yongjiajie.govtech@gmail.com>
2022-07-04 11:58:38 +0800

    refactor: simplify converting cents to dollars and cents string

--- src/utils/currencyFormatter.ts
+++ src/utils/currencyFormatter.ts
@@ -6,17 +6,9 @@
  * @param separator Separator character between dollars and cents
  * @returns Amount in dollars and cents, as a string
  */
-export const formatCentsAsDollarsAndCents = (
-  amountInCents: number,
-  separator = "."
-): string => {
-  const amountString = amountInCents.toString();
-  if (amountString.length < 3) {
-    const centsString = amountString.padStart(2, "0");
-    return `0${separator}${centsString}`;
-  } else {
-    const dollarString = amountString.slice(0, -2);
-    const centsString = amountString.slice(-2);
-    return `${dollarString}${separator}${centsString}`;
-  }
+export const formatCentsAsDollarsAndCents = (amountInCents: number): string => {
+  const amountInDollarsAndCents = amountInCents / 100;
+  return amountInDollarsAndCents.toLocaleString("en-SG", {
+    minimumFractionDigits: 2,
+  });
 };
