228388dbba8d2a3ff88fbf47f702d83dc39b3c00
Adam Chee <adamctj@gmail.com>
2022-08-03 17:59:15 +0800

    feat: change react-navigation part1.2

--- src/components/CustomerQuota/CustomerQuotaProxy.tsx
+++ src/components/CustomerQuota/CustomerQuotaProxy.tsx
@@ -3,22 +3,25 @@ import { CampaignPolicy, CustomerQuotaStackParamList } from "../../types";
 import { ProductContextProvider } from "../../context/products";
 import { CustomerQuotaScreen } from "./CustomerQuotaScreen";
 import { StackScreenProps } from "@react-navigation/stack";
+import { useRoute } from "@react-navigation/native";
 
 type Props = StackScreenProps<
   CustomerQuotaStackParamList,
   "CustomerQuotaProxy"
 >;
 
-export const CustomerQuotaProxy: FunctionComponent<Props> = ({
-  navigation,
-  route,
-}) => {
+export const CustomerQuotaProxy: FunctionComponent<Props> = ({}) => {
   // coming from NRIC screen, it will be a string
   // coming from appeal, it can be an array if group appeal is supported
+  const route = useRoute();
+  console.log("CustomerQuotaProxy, route.params", route.params);
   const navId = route.params?.id;
   const navIds: string[] = Array.isArray(navId) ? navId : [navId];
 
+  console.log("CustomerQuotaProxy, navIds", navIds);
+
   const selectedProducts: CampaignPolicy[] = route.params?.products;
+  console.log("CustomerQuotaProxy, CampaignPolicy[]", selectedProducts);
 
   return (
     <ProductContextProvider products={selectedProducts}>
