289eb3c5010dff0a7d288d4e5eb1951e1c42e6ff
Wynn Chen <wynnchen@Wynns-MacBook-Pro.local>
2022-08-04 16:12:45 +0800

    test: fix

--- src/hooks/govwallet/useGovWalletBalance/useGovWalletBalance.test.tsx
+++ src/hooks/govwallet/useGovWalletBalance/useGovWalletBalance.test.tsx
@@ -18,101 +18,307 @@ const mockGetGovWalletBalance = getGovWalletBalance as jest.Mock;
 const key = "KEY";
 const endpoint = "https://myendpoint.com";
 
-describe("when govwalletExactBalanceValue not -1 or undefined", () => {
-  const testCampaignConfig: CampaignConfig = {
-    ...defaultCampaignConfig,
-    features: {
-      ...defaultFeatures,
-      isPayNowTransaction: true,
-    },
-  };
+describe("useGovWalletBalance", () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+  describe("when govwalletExactBalanceValue not -1 or undefined", () => {
+    const testCampaignConfig: CampaignConfig = {
+      ...defaultCampaignConfig,
+      features: {
+        ...defaultFeatures,
+        isPayNowTransaction: true,
+      },
+    };
 
-  const wrapper: FunctionComponent = ({ children }) => (
-    <ProductContextProvider products={defaultProducts}>
-      <CampaignConfigContextProvider campaignConfig={testCampaignConfig}>
-        {children}
-      </CampaignConfigContextProvider>
-    </ProductContextProvider>
-  );
+    const wrapper: FunctionComponent = ({ children }) => (
+      <ProductContextProvider products={defaultProducts}>
+        <CampaignConfigContextProvider campaignConfig={testCampaignConfig}>
+          {children}
+        </CampaignConfigContextProvider>
+      </ProductContextProvider>
+    );
 
-  const wrapperWithToggleOff: FunctionComponent = ({ children }) => (
-    <ProductContextProvider products={defaultProducts}>
-      <CampaignConfigContextProvider campaignConfig={defaultCampaignConfig}>
-        {children}
-      </CampaignConfigContextProvider>
-    </ProductContextProvider>
-  );
+    const wrapperWithToggleOff: FunctionComponent = ({ children }) => (
+      <ProductContextProvider products={defaultProducts}>
+        <CampaignConfigContextProvider campaignConfig={defaultCampaignConfig}>
+          {children}
+        </CampaignConfigContextProvider>
+      </ProductContextProvider>
+    );
 
-  let ids: string[];
+    let ids: string[];
 
-  describe("isPayNowTransaction toggle", () => {
-    beforeAll(() => {
+    describe("isPayNowTransaction toggle", () => {
+      beforeAll(() => {
+        ids = ["S0000000J"];
+
+        mockGetGovWalletBalance.mockResolvedValue({
+          accountDetails: [
+            {
+              activationStatus: "ACTIVATED",
+              balance: 10000,
+              modified: "2022-07-25T11:57:50.000+08:00",
+            },
+          ],
+        });
+      });
+
+      it("should get balance if toggle is true", async () => {
+        expect.assertions(3);
+
+        const { result, waitForNextUpdate } = renderHook(
+          () => useGovWalletBalance(ids, key, endpoint),
+          { wrapper }
+        );
+
+        expect(result.current.govWalletBalanceState).toStrictEqual(
+          "FETCHING_BALANCE"
+        );
+        await waitForNextUpdate();
+        expect(result.current.govWalletBalanceState).toStrictEqual("ELIGIBLE");
+        expect(mockGetGovWalletBalance).toHaveBeenCalledTimes(1);
+      });
+
+      it("should NOT get balance if toggle is false", async () => {
+        expect.assertions(2);
+
+        const { result } = renderHook(
+          () => useGovWalletBalance(ids, key, endpoint),
+          { wrapper: wrapperWithToggleOff }
+        );
+
+        expect(result.current.govWalletBalanceState).toStrictEqual("DEFAULT");
+        expect(mockGetGovWalletBalance).toHaveBeenCalledTimes(0);
+      });
+    });
+
+    describe("govWalletBalanceState states", () => {
+      let activationStatus: string;
+
+      beforeAll(() => {
+        ids = ["S0000000J"];
+        activationStatus = "ACTIVATED";
+      });
+
+      beforeEach(() => {
+        jest.clearAllMocks();
+      });
+
+      it("should return 'ELIGIBLE' if balance is strictly equal to 10000 cents", async () => {
+        expect.assertions(3);
+
+        mockGetGovWalletBalance.mockResolvedValueOnce({
+          accountDetails: [
+            {
+              activationStatus,
+              balance: 10000,
+              modified: "2022-07-25T11:57:50.000+08:00",
+            },
+          ],
+        });
+
+        const { result, waitForNextUpdate } = renderHook(
+          () => useGovWalletBalance(ids, key, endpoint),
+          { wrapper }
+        );
+
+        expect(result.current.govWalletBalanceState).toStrictEqual(
+          "FETCHING_BALANCE"
+        );
+        await waitForNextUpdate();
+        expect(result.current.govWalletBalanceState).toStrictEqual("ELIGIBLE");
+        expect(mockGetGovWalletBalance).toHaveBeenCalledTimes(1);
+      });
+
+      it("should return 'INELIGIBLE' if balance is more than 10000 cents", async () => {
+        expect.assertions(3);
+
+        mockGetGovWalletBalance.mockResolvedValueOnce({
+          accountDetails: [
+            {
+              activationStatus,
+              balance: 10001,
+              modified: "2022-07-25T11:57:50.000+08:00",
+            },
+          ],
+        });
+
+        const { result, waitForNextUpdate } = renderHook(
+          () => useGovWalletBalance(ids, key, endpoint),
+          { wrapper }
+        );
+
+        expect(result.current.govWalletBalanceState).toStrictEqual(
+          "FETCHING_BALANCE"
+        );
+        await waitForNextUpdate();
+        expect(result.current.govWalletBalanceState).toStrictEqual(
+          "INELIGIBLE"
+        );
+        expect(mockGetGovWalletBalance).toHaveBeenCalledTimes(1);
+      });
+
+      it("should return 'INELIGIBLE' if balance is less than 10000 cents", async () => {
+        expect.assertions(3);
+
+        mockGetGovWalletBalance.mockResolvedValueOnce({
+          accountDetails: [
+            {
+              activationStatus,
+              balance: 9999,
+              modified: "2022-07-25T11:57:50.000+08:00",
+            },
+          ],
+        });
+
+        const { result, waitForNextUpdate } = renderHook(
+          () => useGovWalletBalance(ids, key, endpoint),
+          { wrapper }
+        );
+
+        expect(result.current.govWalletBalanceState).toStrictEqual(
+          "FETCHING_BALANCE"
+        );
+        await waitForNextUpdate();
+        expect(result.current.govWalletBalanceState).toStrictEqual(
+          "INELIGIBLE"
+        );
+        expect(mockGetGovWalletBalance).toHaveBeenCalledTimes(1);
+      });
+
+      it("should return error if account activation status is 'DEACTIVATED'", async () => {
+        expect.assertions(4);
+
+        mockGetGovWalletBalance.mockResolvedValueOnce({
+          accountDetails: [
+            {
+              activationStatus: "DEACTIVATED",
+              balance: 10000,
+              modified: "2022-07-25T11:57:50.000+08:00",
+            },
+          ],
+        });
+
+        const { result, waitForNextUpdate } = renderHook(
+          () => useGovWalletBalance(ids, key, endpoint),
+          { wrapper }
+        );
+
+        expect(result.current.govWalletBalanceState).toStrictEqual(
+          "FETCHING_BALANCE"
+        );
+        await waitForNextUpdate();
+        expect(result.current.govWalletBalanceState).toStrictEqual(
+          "INELIGIBLE"
+        );
+        expect(result.current.govWalletBalanceError).toStrictEqual(
+          new ErrorWithCodes(
+            "Eligible identity's account has been deactivated. Inform your in-charge about this issue.",
+            400
+          )
+        );
+        expect(mockGetGovWalletBalance).toHaveBeenCalledTimes(1);
+      });
+
+      it("should return 'DEFAULT' and error if an error occurred", async () => {
+        expect.assertions(4);
+
+        mockGetGovWalletBalance.mockRejectedValue(
+          new NetworkError("Network error")
+        );
+
+        const { result, waitForNextUpdate } = renderHook(
+          () => useGovWalletBalance(ids, key, endpoint),
+          { wrapper }
+        );
+
+        expect(result.current.govWalletBalanceState).toStrictEqual(
+          "FETCHING_BALANCE"
+        );
+        await waitForNextUpdate();
+        expect(result.current.govWalletBalanceState).toStrictEqual("DEFAULT");
+        expect(mockGetGovWalletBalance).toHaveBeenCalledTimes(1);
+        expect(result.current.govWalletBalanceError).toStrictEqual(
+          new NetworkError("Network error")
+        );
+      });
+
+      it("should return 'DEFAULT' and error if a HTTP 400 error occurred", async () => {
+        expect.assertions(4);
+
+        mockGetGovWalletBalance.mockRejectedValue(
+          new ErrorWithCodes("Invalid customerId", 400, "F00B4R")
+        );
+
+        const { result, waitForNextUpdate } = renderHook(
+          () => useGovWalletBalance(ids, key, endpoint),
+          { wrapper }
+        );
+
+        expect(result.current.govWalletBalanceState).toStrictEqual(
+          "FETCHING_BALANCE"
+        );
+        await waitForNextUpdate();
+        expect(result.current.govWalletBalanceState).toStrictEqual("DEFAULT");
+        expect(mockGetGovWalletBalance).toHaveBeenCalledTimes(1);
+        expect(result.current.govWalletBalanceError).toStrictEqual(
+          new ErrorWithCodes("Invalid customerId", 400, "F00B4R")
+        );
+      });
+
+      it("should return 'DEFAULT' and error if a HTTP 500 error occurred", async () => {
+        expect.assertions(4);
+
+        mockGetGovWalletBalance.mockRejectedValue(new ErrorWithCodes("", 500));
+
+        const { result, waitForNextUpdate } = renderHook(
+          () => useGovWalletBalance(ids, key, endpoint),
+          { wrapper }
+        );
+
+        expect(result.current.govWalletBalanceState).toStrictEqual(
+          "FETCHING_BALANCE"
+        );
+        await waitForNextUpdate();
+        expect(result.current.govWalletBalanceState).toStrictEqual("DEFAULT");
+        expect(mockGetGovWalletBalance).toHaveBeenCalledTimes(1);
+        expect(result.current.govWalletBalanceError).toStrictEqual(
+          new ErrorWithCodes("", 500)
+        );
+      });
+    });
+  });
+
+  describe("when govwalletExactBalanceValue -1 or undefined", () => {
+    const testCampaignConfig: CampaignConfig = {
+      ...defaultCampaignConfig,
+      features: {
+        ...defaultFeatures,
+        isPayNowTransaction: true,
+        govwalletExactBalanceValue: -1,
+      },
+    };
+
+    const wrapper: FunctionComponent = ({ children }) => (
+      <ProductContextProvider products={defaultProducts}>
+        <CampaignConfigContextProvider campaignConfig={testCampaignConfig}>
+          {children}
+        </CampaignConfigContextProvider>
+      </ProductContextProvider>
+    );
+
+    let ids: string[];
+
+    it("should not check balance exact match", async () => {
+      expect.assertions(3);
       ids = ["S0000000J"];
 
       mockGetGovWalletBalance.mockResolvedValue({
         accountDetails: [
           {
             activationStatus: "ACTIVATED",
-            balance: 10000,
-            modified: "2022-07-25T11:57:50.000+08:00",
-          },
-        ],
-      });
-    });
-
-    beforeEach(() => {
-      jest.clearAllMocks();
-    });
-
-    it("should get balance if toggle is true", async () => {
-      expect.assertions(3);
-
-      const { result, waitForNextUpdate } = renderHook(
-        () => useGovWalletBalance(ids, key, endpoint),
-        { wrapper }
-      );
-
-      expect(result.current.govWalletBalanceState).toStrictEqual(
-        "FETCHING_BALANCE"
-      );
-      await waitForNextUpdate();
-      expect(result.current.govWalletBalanceState).toStrictEqual("ELIGIBLE");
-      expect(mockGetGovWalletBalance).toHaveBeenCalledTimes(1);
-    });
-
-    it("should NOT get balance if toggle is false", async () => {
-      expect.assertions(2);
-
-      const { result } = renderHook(
-        () => useGovWalletBalance(ids, key, endpoint),
-        { wrapper: wrapperWithToggleOff }
-      );
-
-      expect(result.current.govWalletBalanceState).toStrictEqual("DEFAULT");
-      expect(mockGetGovWalletBalance).toHaveBeenCalledTimes(0);
-    });
-  });
-
-  describe("govWalletBalanceState states", () => {
-    let activationStatus: string;
-
-    beforeAll(() => {
-      ids = ["S0000000J"];
-      activationStatus = "ACTIVATED";
-    });
-
-    beforeEach(() => {
-      jest.clearAllMocks();
-    });
-
-    it("should return 'ELIGIBLE' if balance is strictly equal to 10000 cents", async () => {
-      expect.assertions(3);
-
-      mockGetGovWalletBalance.mockResolvedValueOnce({
-        accountDetails: [
-          {
-            activationStatus,
-            balance: 10000,
+            balance: 100000,
             modified: "2022-07-25T11:57:50.000+08:00",
           },
         ],
@@ -130,204 +336,5 @@ describe("when govwalletExactBalanceValue not -1 or undefined", () => {
       expect(result.current.govWalletBalanceState).toStrictEqual("ELIGIBLE");
       expect(mockGetGovWalletBalance).toHaveBeenCalledTimes(1);
     });
-
-    it("should return 'INELIGIBLE' if balance is more than 10000 cents", async () => {
-      expect.assertions(3);
-
-      mockGetGovWalletBalance.mockResolvedValueOnce({
-        accountDetails: [
-          {
-            activationStatus,
-            balance: 10001,
-            modified: "2022-07-25T11:57:50.000+08:00",
-          },
-        ],
-      });
-
-      const { result, waitForNextUpdate } = renderHook(
-        () => useGovWalletBalance(ids, key, endpoint),
-        { wrapper }
-      );
-
-      expect(result.current.govWalletBalanceState).toStrictEqual(
-        "FETCHING_BALANCE"
-      );
-      await waitForNextUpdate();
-      expect(result.current.govWalletBalanceState).toStrictEqual("INELIGIBLE");
-      expect(mockGetGovWalletBalance).toHaveBeenCalledTimes(1);
-    });
-
-    it("should return 'INELIGIBLE' if balance is less than 10000 cents", async () => {
-      expect.assertions(3);
-
-      mockGetGovWalletBalance.mockResolvedValueOnce({
-        accountDetails: [
-          {
-            activationStatus,
-            balance: 9999,
-            modified: "2022-07-25T11:57:50.000+08:00",
-          },
-        ],
-      });
-
-      const { result, waitForNextUpdate } = renderHook(
-        () => useGovWalletBalance(ids, key, endpoint),
-        { wrapper }
-      );
-
-      expect(result.current.govWalletBalanceState).toStrictEqual(
-        "FETCHING_BALANCE"
-      );
-      await waitForNextUpdate();
-      expect(result.current.govWalletBalanceState).toStrictEqual("INELIGIBLE");
-      expect(mockGetGovWalletBalance).toHaveBeenCalledTimes(1);
-    });
-
-    it("should return error if account activation status is 'DEACTIVATED'", async () => {
-      expect.assertions(4);
-
-      mockGetGovWalletBalance.mockResolvedValueOnce({
-        accountDetails: [
-          {
-            activationStatus: "DEACTIVATED",
-            balance: 10000,
-            modified: "2022-07-25T11:57:50.000+08:00",
-          },
-        ],
-      });
-
-      const { result, waitForNextUpdate } = renderHook(
-        () => useGovWalletBalance(ids, key, endpoint),
-        { wrapper }
-      );
-
-      expect(result.current.govWalletBalanceState).toStrictEqual(
-        "FETCHING_BALANCE"
-      );
-      await waitForNextUpdate();
-      expect(result.current.govWalletBalanceState).toStrictEqual("INELIGIBLE");
-      expect(result.current.govWalletBalanceError).toStrictEqual(
-        new ErrorWithCodes(
-          "Eligible identity's account has been deactivated. Inform your in-charge about this issue.",
-          400
-        )
-      );
-      expect(mockGetGovWalletBalance).toHaveBeenCalledTimes(1);
-    });
-
-    it("should return 'DEFAULT' and error if an error occurred", async () => {
-      expect.assertions(4);
-
-      mockGetGovWalletBalance.mockRejectedValue(
-        new NetworkError("Network error")
-      );
-
-      const { result, waitForNextUpdate } = renderHook(
-        () => useGovWalletBalance(ids, key, endpoint),
-        { wrapper }
-      );
-
-      expect(result.current.govWalletBalanceState).toStrictEqual(
-        "FETCHING_BALANCE"
-      );
-      await waitForNextUpdate();
-      expect(result.current.govWalletBalanceState).toStrictEqual("DEFAULT");
-      expect(mockGetGovWalletBalance).toHaveBeenCalledTimes(1);
-      expect(result.current.govWalletBalanceError).toStrictEqual(
-        new NetworkError("Network error")
-      );
-    });
-
-    it("should return 'DEFAULT' and error if a HTTP 400 error occurred", async () => {
-      expect.assertions(4);
-
-      mockGetGovWalletBalance.mockRejectedValue(
-        new ErrorWithCodes("Invalid customerId", 400, "F00B4R")
-      );
-
-      const { result, waitForNextUpdate } = renderHook(
-        () => useGovWalletBalance(ids, key, endpoint),
-        { wrapper }
-      );
-
-      expect(result.current.govWalletBalanceState).toStrictEqual(
-        "FETCHING_BALANCE"
-      );
-      await waitForNextUpdate();
-      expect(result.current.govWalletBalanceState).toStrictEqual("DEFAULT");
-      expect(mockGetGovWalletBalance).toHaveBeenCalledTimes(1);
-      expect(result.current.govWalletBalanceError).toStrictEqual(
-        new ErrorWithCodes("Invalid customerId", 400, "F00B4R")
-      );
-    });
-
-    it("should return 'DEFAULT' and error if a HTTP 500 error occurred", async () => {
-      expect.assertions(4);
-
-      mockGetGovWalletBalance.mockRejectedValue(new ErrorWithCodes("", 500));
-
-      const { result, waitForNextUpdate } = renderHook(
-        () => useGovWalletBalance(ids, key, endpoint),
-        { wrapper }
-      );
-
-      expect(result.current.govWalletBalanceState).toStrictEqual(
-        "FETCHING_BALANCE"
-      );
-      await waitForNextUpdate();
-      expect(result.current.govWalletBalanceState).toStrictEqual("DEFAULT");
-      expect(mockGetGovWalletBalance).toHaveBeenCalledTimes(1);
-      expect(result.current.govWalletBalanceError).toStrictEqual(
-        new ErrorWithCodes("", 500)
-      );
-    });
-  });
-});
-
-describe("when govwalletExactBalanceValue -1 or undefined", () => {
-  const testCampaignConfig: CampaignConfig = {
-    ...defaultCampaignConfig,
-    features: {
-      ...defaultFeatures,
-      isPayNowTransaction: true,
-      govwalletExactBalanceValue: -1,
-    },
-  };
-
-  const wrapper: FunctionComponent = ({ children }) => (
-    <ProductContextProvider products={defaultProducts}>
-      <CampaignConfigContextProvider campaignConfig={testCampaignConfig}>
-        {children}
-      </CampaignConfigContextProvider>
-    </ProductContextProvider>
-  );
-
-  let ids: string[];
-
-  it("should not check balance exact match", async () => {
-    expect.assertions(3);
-    ids = ["S0000000J"];
-
-    mockGetGovWalletBalance.mockResolvedValue({
-      accountDetails: [
-        {
-          activationStatus: "ACTIVATED",
-          balance: 100000,
-          modified: "2022-07-25T11:57:50.000+08:00",
-        },
-      ],
-    });
-
-    const { result, waitForNextUpdate } = renderHook(
-      () => useGovWalletBalance(ids, key, endpoint),
-      { wrapper }
-    );
-
-    expect(result.current.govWalletBalanceState).toStrictEqual(
-      "FETCHING_BALANCE"
-    );
-    await waitForNextUpdate();
-    expect(result.current.govWalletBalanceState).toStrictEqual("ELIGIBLE");
-    expect(mockGetGovWalletBalance).toHaveBeenCalledTimes(1);
   });
 });
