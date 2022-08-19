4365def2e9e290a4036938cda0cf4d0a0676ba13
Wen Qing <leongwenqing@gmail.com>
2022-08-19 16:29:23 +0800

    chore: remove unused imports

--- src/navigation/Content.tsx
+++ src/navigation/Content.tsx
@@ -21,7 +21,6 @@ import {
   TransitionPresets,
   StackNavigationOptions,
 } from "@react-navigation/stack";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { CampaignInitialisationScreen } from "../components/CampaignInitialisation/CampaignInitialisationScreen";
 import { CampaignLocationsScreen } from "../components/CampaignLocations/CampaignLocationsScreen";
 import { updateI18nLocale } from "../common/i18n/i18nSetup";
