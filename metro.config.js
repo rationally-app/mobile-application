b760ee58ae720711d46a424ff61cca2f2a08133d
wynn <wynn987@gmail.com>
2022-10-07 12:55:58 +0800

    chore: add dist

--- metro.config.js
+++ metro.config.js
@@ -1,12 +1,13 @@
-const { getDefaultConfig } = require("metro-config");
+const { getDefaultConfig } = require("expo/metro-config");
 
 module.exports = (async () => {
   const {
     resolver: { sourceExts, assetExts },
-  } = await getDefaultConfig();
+  } = await getDefaultConfig(__dirname);
   return {
     transformer: {
       babelTransformerPath: require.resolve("react-native-svg-transformer"),
+      assetPlugins: ['expo-asset/tools/hashAssetFiles'],
     },
     resolver: {
       assetExts: assetExts.filter((ext) => ext !== "svg"),
