fe0dee754f94bcfd464b7a9d0ea75af61b48cda0
wynn <wynn987@gmail.com>
2022-10-13 10:45:42 +0800

    chore: fix storybook

--- src/config/index.tsx
+++ src/config/index.tsx
@@ -1,9 +1,9 @@
 import Constants from "expo-constants";
 
-export const SENTRY_DSN: string = Constants.manifest?.extra?.sentryDsn;
-export const IS_STORYBOOK_VIEW = Constants.manifest?.extra?.storybook;
-export const IS_MOCK = Constants.manifest?.extra?.mock;
+export const SENTRY_DSN: string = Constants.manifest2?.extra?.expoClient?.extra?.eas?.sentryDsn;
+export const IS_STORYBOOK_VIEW = Constants.manifest2?.extra?.expoClient?.extra?.eas?.storybook;
+export const IS_MOCK = Constants.manifest2?.extra?.expoClient?.extra?.eas?.mock;
 export const APP_BINARY_VERSION = Constants.manifest?.version as string;
 export const APP_BUILD_VERSION: number =
-  Constants.manifest?.extra?.appBuildVersion;
-export const DOMAIN_FORMAT: string = Constants.manifest?.extra?.domainFormat;
+  Constants.manifest2?.extra?.expoClient?.extra?.eas?.appBuildVersion;
+export const DOMAIN_FORMAT: string = Constants.manifest2?.extra?.expoClient?.extra?.eas?.domainFormat;
