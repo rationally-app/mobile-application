81c51b94671973abed9b2801793a60444e5198fb
wynn <wynn987@gmail.com>
2022-10-13 12:24:57 +0800

    chore: lint fix

--- src/config/index.tsx
+++ src/config/index.tsx
@@ -1,9 +1,13 @@
 import Constants from "expo-constants";
 
-export const SENTRY_DSN: string = Constants.manifest2?.extra?.expoClient?.extra?.sentryDsn;
-export const IS_STORYBOOK_VIEW = Constants.manifest2?.extra?.expoClient?.extra?.storybook;
+export const SENTRY_DSN: string =
+  Constants.manifest2?.extra?.expoClient?.extra?.sentryDsn;
+export const IS_STORYBOOK_VIEW =
+  Constants.manifest2?.extra?.expoClient?.extra?.storybook;
 export const IS_MOCK = Constants.manifest2?.extra?.expoClient?.extra?.mock;
-export const APP_BINARY_VERSION = Constants.manifest2?.extra?.expoClient?.version as string;
+export const APP_BINARY_VERSION = Constants.manifest2?.extra?.expoClient
+  ?.version as string;
 export const APP_BUILD_VERSION: number =
   Constants.manifest2?.extra?.expoClient?.extra?.appBuildVersion;
-export const DOMAIN_FORMAT: string = Constants.manifest2?.extra?.expoClient?.extra?.domainFormat;
+export const DOMAIN_FORMAT: string =
+  Constants.manifest2?.extra?.expoClient?.extra?.domainFormat;
