57aaa4e3d6d9e119b87c3d6390184ffdcb065079
wynn <wynn987@gmail.com>
2022-10-19 17:50:05 +0800

    chore: fix env vars

--- metro.config.js
+++ metro.config.js
@@ -1,16 +1,16 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const { getDefaultConfig } = require("expo/metro-config");
 
-module.exports = (async () => {
-  const {
-    resolver: { sourceExts, assetExts },
-  } = await getDefaultConfig(__dirname);
-  return {
-    transformer: {
-      babelTransformerPath: require.resolve("react-native-svg-transformer"),
-    },
-    resolver: {
-      assetExts: assetExts.filter((ext) => ext !== "svg"),
-      sourceExts: [...sourceExts, "svg"],
-    },
-  };
-})();
+const config = getDefaultConfig(__dirname);
+
+config.transformer = {
+  ...config.transformer,
+  babelTransformerPath: require.resolve("react-native-svg-transformer"),
+};
+
+config.resolver = {
+  assetExts: config.resolver.assetExts.filter((ext) => ext !== "svg"),
+  sourceExts: [...config.resolver.sourceExts, "svg"],
+};
+
+module.exports = config;
--- src/config/index.tsx
+++ src/config/index.tsx
@@ -1,13 +1,21 @@
 import Constants from "expo-constants";
 
-export const SENTRY_DSN: string =
-  Constants.manifest2?.extra?.expoClient?.extra?.sentryDsn;
-export const IS_STORYBOOK_VIEW =
-  Constants.manifest2?.extra?.expoClient?.extra?.storybook;
-export const IS_MOCK = Constants.manifest2?.extra?.expoClient?.extra?.mock;
-export const APP_BINARY_VERSION = Constants.manifest2?.extra?.expoClient
-  ?.version as string;
-export const APP_BUILD_VERSION: number =
-  Constants.manifest2?.extra?.expoClient?.extra?.appBuildVersion;
-export const DOMAIN_FORMAT: string =
-  Constants.manifest2?.extra?.expoClient?.extra?.domainFormat;
+export const SENTRY_DSN: string = Constants.manifest?.extra?.sentryDsn
+  ? Constants.manifest?.extra?.sentryDsn
+  : Constants.manifest2?.extra?.expoClient?.extra?.sentryDsn;
+export const IS_STORYBOOK_VIEW = Constants.manifest?.extra?.storybook
+  ? Constants.manifest?.extra?.storybook
+  : Constants.manifest2?.extra?.expoClient?.extra?.storybook;
+export const IS_MOCK = Constants.manifest?.extra?.mock
+  ? Constants.manifest?.extra?.mock
+  : Constants.manifest2?.extra?.expoClient?.extra?.mock;
+export const APP_BINARY_VERSION = Constants.manifest?.version
+  ? (Constants.manifest?.version as string)
+  : (Constants.manifest2?.extra?.expoClient?.version as string);
+export const APP_BUILD_VERSION: number = Constants.manifest?.extra
+  ?.appBuildVersion
+  ? Constants.manifest?.extra?.appBuildVersion
+  : Constants.manifest2?.extra?.expoClient?.extra?.appBuildVersion;
+export const DOMAIN_FORMAT: string = Constants.manifest?.extra?.domainFormat
+  ? Constants.manifest?.extra?.domainFormat
+  : Constants.manifest2?.extra?.expoClient?.extra?.domainFormat;
