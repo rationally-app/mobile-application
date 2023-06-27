a5eba8e907f3be2ce4a16c29c0198e00cc513b9f
Raphael <raphaeltanyw@gmail.com>
2023-06-27 14:38:06 +0800

    chore: expo 47 upgrade (#745)

--- src/hooks/useTranslate/useTranslate.ts
+++ src/hooks/useTranslate/useTranslate.ts
@@ -33,8 +33,8 @@ export const i18ntWithValidator = <
   dynamicContent?: Record<string, string | number>
 ): string => {
   return i18n.t(
-    `${component}.${subComponent}${
-      subSubComponent ? `.${subSubComponent}` : ""
+    `${component}.${String(subComponent)}${
+      subSubComponent ? `.${String(subSubComponent)}` : ""
     }`,
     dynamicContent
   );
